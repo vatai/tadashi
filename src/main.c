0db21ff805264e2f823245e2387a61604884d333
Emil VATAI <emil.vatai@gmail.com>
2022-02-21 09:52:59 +0900

    Add printing to `main.c`

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdio.h>
 
 #include <isl/aff.h>
 #include <isl/arg.h>
@@ -32,19 +33,53 @@ ISL_ARGS_END
 ISL_ARG_DEF(options, struct options, options_args)
 
 int main(int argc, char *argv[]) {
+  struct options *options;
+  char *output;
+  isl_ctx *ctx;
+  pet_scop *scop;
+  isl_printer *p;
 
-  struct options *options = options_new_with_defaults();
-  isl_ctx *ctx = isl_ctx_alloc_with_options(&options_args, options);
+  options = options_new_with_defaults();
+  ctx = isl_ctx_alloc_with_options(&options_args, options);
   assert(ctx != NULL);
 
-  pet_scop *scop = pet_scop_extract_from_C_source(ctx, filename, NULL);
+  scop = pet_scop_extract_from_C_source(ctx, filename, NULL);
   assert(scop != NULL);
 
-  isl_printer *p = isl_printer_to_str(ctx);
+  p = isl_printer_to_str(ctx);
   assert(p != NULL);
+  output = isl_printer_get_str(p);
+  printf("%s\n", output);
 
-  p = isl_printer_print_set(p, scop->context_value);
-  // char *output = isl_printer_get_str(isl_prn);
+  printf("n_arrays: %d\n", scop->n_array);
+  for (int i = 0; i < scop->n_array; ++i) {
+    struct pet_array *array = scop->arrays[i];
+    p = isl_printer_print_set(p, array->context);
+    output = isl_printer_get_str(p);
+    printf("arrays[i]->context: %s\n", output);
+    p = isl_printer_print_set(p, array->extent);
+    output = isl_printer_get_str(p);
+    printf("arrays[i]->extent: %s\n", output);
+  }
+
+  printf("n_implication: %d\n", scop->n_implication);
+  for (int i = 0; i < scop->n_implication; ++i) {
+    p = isl_printer_print_map(p, scop->implications[i]->extension);
+    output = isl_printer_get_str(p);
+    printf("implications[i]->extension: %s\n", output);
+    printf("satisfied: %d\n", scop->implications[i]->satisfied);
+  }
+
+  printf("n_independence: %d\n", scop->n_independence);
+  for (int i = 0; i < scop->n_independence; ++i) {
+    p = isl_printer_print_union_map(p, scop->independences[i]->filter);
+    output = isl_printer_get_str(p);
+    printf("independences[i]->filter: %s\n", output);
+    p = isl_printer_print_union_set(p, scop->independences[i]->local);
+    output = isl_printer_get_str(p);
+    printf("independences[i]->local: %s\n", output);
+  }
+
   // std::cout << output << std::endl;
   // isl_printer_free(isl_prn);
 
