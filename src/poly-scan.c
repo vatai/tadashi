598f32bbeaf7db8a9491400d6010a6d5507a5f4e
Emil VATAI <emil.vatai@gmail.com>
2022-04-21 10:58:19 +0900

    WIP intersection

--- src/poly-scan.c
+++ src/poly-scan.c
@@ -81,6 +81,7 @@ __isl_give isl_basic_set *build_bset1(__isl_keep isl_ctx *ctx, //
   isl_basic_set *bset;
   space = isl_space_unit(ctx);
   space = isl_space_add_param_id(space, N_id);
+  space = isl_space_add_param_id(space, M_id);
   space = isl_space_add_unnamed_tuple_ui(space, 2);
   ma = isl_multi_aff_identity_on_domain_space(isl_space_copy(space));
   ma = isl_multi_aff_set_dim_name(ma, isl_dim_in, 0, "i");
@@ -92,7 +93,7 @@ __isl_give isl_basic_set *build_bset1(__isl_keep isl_ctx *ctx, //
   bset = isl_aff_ge_basic_set(isl_aff_copy(var), cst);
 
   cst = isl_aff_param_on_domain_space_id(isl_space_copy(space),
-                                         isl_id_copy(N_id));
+                                         isl_id_copy(M_id));
   bset = isl_basic_set_intersect(bset, isl_aff_le_basic_set(var, cst));
 
   var = isl_multi_aff_get_at(ma, 1);
@@ -102,8 +103,8 @@ __isl_give isl_basic_set *build_bset1(__isl_keep isl_ctx *ctx, //
       bset, isl_aff_eq_basic_set(isl_aff_copy(var), isl_aff_copy(cst)));
 
   isl_aff_free(var);
-  isl_multi_aff_free(ma);
   isl_aff_free(cst);
+  isl_multi_aff_free(ma);
   isl_space_free(space);
   return bset;
 }
@@ -133,21 +134,42 @@ int main(int argc, char *argv[]) {
     isl_space_free(spaces[i]);
   // Spaces end
 
+  isl_basic_set *bsets[3];
   isl_basic_set *bset;
-  bset = build_bset0(ctx, N_id, i_id, j_id);
-  printf("bset: %s\n", isl_basic_set_to_str(bset));
-  isl_basic_set_free(bset);
-
-  bset = build_bset1(ctx, N_id, i_id);
-  printf("bset: %s\n", isl_basic_set_to_str(bset));
-  isl_basic_set_free(bset);
-
-  bset = isl_basic_set_read_from_str(
+  bsets[0] = build_bset0(ctx, N_id, i_id, j_id);
+  bsets[1] = build_bset1(ctx, N_id, M_id);
+  bsets[2] = isl_basic_set_read_from_str(
       ctx, "[N] -> { [i, j] : j = i and 0 < i <= N }");
-  printf("bset: %s\n", isl_basic_set_to_str(bset));
-  isl_basic_set_free(bset);
+  for (int i = 0; i < 3; i++)
+    printf("bset: %s\n", isl_basic_set_to_str(bsets[i]));
 
-  // bset = isl_basic_set_project_out(bset, isl_dim_set, 1, 1);
+  for (int first = 0; first < 2; first++) {
+    for (int n = 0; n <= 2 - first; n++) {
+      bset = isl_basic_set_project_out(isl_basic_set_copy(bsets[1]),
+                                       isl_dim_set, first, n);
+      printf("bset proj (first=%d, n=%d): %s\n", first, n,
+             isl_basic_set_to_str(bset));
+      isl_basic_set_free(bset);
+    }
+  }
+
+  isl_basic_set *bset1, *bset2;
+  bset1 = isl_basic_set_project_out( //
+      isl_basic_set_copy(bsets[0]), isl_dim_set, 0, 1);
+  printf("bset1: %s\n", isl_basic_set_to_str(bset1));
+  bset2 = isl_basic_set_project_out( //
+      isl_basic_set_copy(bsets[1]), isl_dim_set, 0, 1);
+  printf("bset2: %s\n", isl_basic_set_to_str(bset2));
+  /* bset = isl_basic_set_intersect(isl_basic_set_copy(bset1), */
+  /*                                isl_basic_set_copy(bset2)); */
+  /* printf("intersection: %s\n", isl_basic_set_to_str(bset)); */
+  isl_basic_set_free(bset1);
+  isl_basic_set_free(bset2);
+  /* isl_basic_set_free(bset); */
+
+  // Free stuff
+  for (int i = 0; i < 3; i++)
+    isl_basic_set_free(bsets[i]);
 
   isl_id_free(N_id);
   isl_id_free(M_id);
