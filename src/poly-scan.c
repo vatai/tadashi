a03eb1f1d1f859a05a2cbd268d960d1674582cf4
Emil VATAI <emil.vatai@gmail.com>
2022-05-19 22:59:48 +0900

    Start `schedule()`

--- src/poly-scan.c
+++ src/poly-scan.c
@@ -6,9 +6,12 @@
 #include <isl/constraint.h>
 #include <isl/id.h>
 #include <isl/map.h>
+#include <isl/schedule.h>
 #include <isl/set.h>
 #include <isl/set_type.h>
 #include <isl/space.h>
+#include <isl/union_map.h>
+#include <isl/union_set.h>
 
 void inspect_spaces(__isl_keep isl_ctx *ctx) {
   printf("--- inspect_spaces(ctx) ---\n\n");
@@ -196,12 +199,75 @@ void project_and_intersect(__isl_keep isl_ctx *ctx) {
   isl_id_free(j_id);
 }
 
+void schedule(__isl_keep isl_ctx *ctx) {
+  printf("--- schedule(ctx) ---\n\n");
+  isl_space *space;
+  isl_multi_aff *ma;
+  isl_aff *i_aff, *j_aff;
+  isl_val *zero = isl_val_int_from_si(ctx, 0);
+  isl_val *one = isl_val_int_from_si(ctx, 1);
+
+  /* isl_id *N_id = isl_id_alloc(ctx, "N", NULL); */
+  /* isl_id *M_id = isl_id_alloc(ctx, "M", NULL); */
+
+  isl_schedule *schedule;
+  isl_set *domain;
+  isl_union_map *validity, *proximity;
+
+  space = isl_space_params_alloc(ctx, 2);
+  space = isl_space_add_unnamed_tuple_ui(space, 2);
+  /* space = isl_space_add_param_id(space, N_id); */
+  /* space = isl_space_add_param_id(space, M_id); */
+  space = isl_space_set_dim_name(space, isl_dim_param, 0, "N");
+  space = isl_space_set_dim_name(space, isl_dim_param, 1, "M");
+  space = isl_space_set_dim_name(space, isl_dim_set, 0, "i");
+  space = isl_space_set_dim_name(space, isl_dim_set, 1, "j");
+  printf("space: %s\n", isl_space_to_str(space));
+  // space: [N, M] -> { [i, j] }
+
+  /* ma = isl_multi_aff_identity_on_domain_space(isl_space_copy(space)); */
+  /* i_aff = isl_multi_aff_get_at(ma, 0); */
+  /* j_aff = isl_multi_aff_get_at(ma, 1); */
+  /* printf("ma: %s\n", isl_multi_aff_to_str(ma)); */
+  /* printf("i_aff: %s\n", isl_aff_to_str(i_aff)); */
+  /* printf("j_aff: %s\n", isl_aff_to_str(j_aff)); */
+  /* domain = isl_aff_eq_basic_set(i_aff, cst); */
+  /* domain = */
+  /*     isl_set_read_from_str(ctx, "[N] -> { [i, j] : j = i and 0 < i <= N}");
+   */
+  char validity_str[] = "[N] -> { [i0] -> [o0] : 1 <= i0 <= N }";
+  char proximity_str[] = "[N] -> { [i0] -> [o0] : 1 <= i0 <= N }";
+  /* validity = isl_union_map_read_from_str(ctx, validity_str); */
+  /* proximity = isl_union_map_read_from_str(ctx, proximity_str); */
+
+  /* printf("domain: %s\n", isl_set_to_str(domain)); */
+  /* printf("validity: %s\n", isl_union_map_to_str(validity)); */
+  /* printf("proximity: %s\n", isl_union_map_to_str(proximity)); */
+
+  /* schedule = isl_union_set_compute_schedule(domain, validity, proximity); */
+  /* printf("schedule: %s\n", isl_schedule_to_str(schedule)); */
+
+  /* isl_set_free(domain); */
+  /* isl_union_map_free(validity); */
+  /* isl_union_map_free(proximity); */
+
+  isl_space_free(space);
+  /* isl_id_free(N_id); */
+  /* isl_id_free(M_id); */
+  /* isl_multi_aff_free(ma); */
+  /* isl_aff_free(i_aff); */
+  /* isl_aff_free(j_aff); */
+  isl_val_free(zero);
+  isl_val_free(one);
+}
+
 int main(int argc, char *argv[]) {
 
   isl_ctx *ctx = isl_ctx_alloc();
 
   inspect_spaces(ctx);
   project_and_intersect(ctx);
+  schedule(ctx);
 
   isl_ctx_free(ctx);
 
