0e4951f85e0f036cf5abccaae507e7d6da54c32f
Emil VATAI <emil.vatai@gmail.com>
2022-12-27 20:12:13 +0900

    Iniital version of pluto.sh

--- deps/pluto.sh
+++ deps/pluto.sh
@@ -2,69 +2,83 @@
 set -x
 set -e
 
-ROOT=llvm-for-poly
-BRANCH=llvmorg-10.0.0
-PREFIX="$(pwd)/${ROOT}/opt"
+mkdir_ok_if_exists ()
+{
+    [ -e "$1" ] || mkdir -p "$1"
+}
+
+ROOT=$(pwd)
+DOWNLOAD="${ROOT}/downloads"
+OPT="${ROOT}/opt"
+SRC="${ROOT}/src"
+PATCH="${ROOT}/patches"
+BUILD="${ROOT}/build"
+mkdir_ok_if_exists "$DOWNLOAD"
+mkdir_ok_if_exists "${OPT}"
+mkdir_ok_if_exists "${SRC}"
+mkdir_ok_if_exists "${PATCH}"
+mkdir_ok_if_exists "${BUILD}"
+
+LLVM_BRANCH="9.0.1"
+LLVM_EXT="tar.xz"
+LLVM_FILE="llvm-project-${LLVM_BRANCH}"
+LLVM_URL_DIR="https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_BRANCH}"
+LLVM_URL="${LLVM_URL_DIR}/${LLVM_FILE}.${LLVM_EXT}"
+LLVM_PREFIX="${OPT}/${LLVM_FILE}"
+LLVM_EXTRACTED="llvm-project-${LLVM_BRANCH}"
+
 cmake_args=(
     -G Ninja
     -DCMAKE_BUILD_TYPE=Release
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-    # -DLLVM_ENABLE_PROJECTS=';clang-tools-extra;compiler-rt;lld;polly;debuginfo-tests;libunwind;lldb'
-    -DLLVM_ENABLE_PROJECTS='clang;libcxxabi;libcxx'
-    # -DLLVM_BUILD_LLVM_DYLIB=ON
-    # -DLLVM_LINK_LLVM_DYLIB=ON
-    -DLLVM_BUILD_STATIC=ON
-    -DLLVM_INSTALL_UTILS=ON
-    -DLLVM_ENABLE_RTTI=ON
-    -DLLVM_ENABLE_FFI=ON
-    # -DLLVM_BUILD_TESTS=ON
+    -DCMAKE_INSTALL_PREFIX=${LLVM_PREFIX}
     -DLLVM_PARALLEL_LINK_JOBS=1
-    # -DCMAKE_CXX_FLAGS=-stdlib=libc++
-    # -DLLVM_BINUTILS_INCDIR=/usr/include
+    -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld"
+    -DLLVM_BUILD_LLVM_DYLIB=ON
 )
 
-CLANG_PREFIX="${HOME}/other-code/llvm-for-poly/opt/"
+pushd "${DOWNLOAD}"
+wget -nc "${LLVM_URL}"
+popd
 
-# rm -fr pluto
-# git clone git@github.com:bondhugula/pluto.git
+pushd "${PATCH}"
+wget -nc https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/llvm11/trunk/utils-benchmark-fix-missing-include.patch
+popd
 
-# rm -rf ${ROOT}
-# git clone -b ${BRANCH} https://github.com/llvm/llvm-project.git ${ROOT}
-wget https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/llvm11/trunk/utils-benchmark-fix-missing-include.patch
+pushd "${BUILD}"
+[ -e "${LLVM_EXTRACTED}" ] || tar xvf "${DOWNLOAD}/${LLVM_FILE}.${LLVM_EXT}"
+pushd "${LLVM_EXTRACTED}"
 
-
-
-pushd ${ROOT}
-
-rm -rf build "${PREFIX}"
-
-git checkout ${BRANCH} ############
-git reset --hard
-patch -Np1 -i ../utils-benchmark-fix-missing-include.patch
-
+mkdir_ok_if_exists build
+# patch -Np1 -i ../utils-benchmark-fix-missing-include.patch
 # CC=clang CXX=clang++
 cmake -S llvm -B build "${cmake_args[@]}"
 cmake --build build
 ninja -C build install
 
 popd
-echo ">>> Finished building LLVM! <<<"
+popd
 
-
-pushd pluto
-
-git submodule init
-git submodule update
+# rm -fr pluto
+pushd ${BUILD}
+git clone git@github.com:bondhugula/pluto.git
+git submodule init --update
 
 ./autogen.sh
 
 configure_args=(
+    --prefix=${PLUTO_PREFIX}
     --enable-debug
-    --with-clang-prefix=${CLANG_PREFIX}
-    )
+    --with-clang-prefix=${LLVM_PREFIX}
+    --with-pet=bundled
+    --with-isl=bundled
+    --enable-shared-barvinok
+)
 
 ./configure "${configure_args[@]}"
 make -j
 make -j test
-
+pushd pluto
 popd
+popd
+
+
