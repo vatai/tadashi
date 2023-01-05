f221be941fc74ea083c9f9ffae7618e6a05cfdb8
Emil VATAI <emil.vatai@gmail.com>
2023-01-05 15:01:08 +0900

    Clean up pluto.sh

--- deps/pluto.sh
+++ deps/pluto.sh
@@ -7,39 +7,20 @@ set_env "${LLVM_PREFIX}"
 
 PLUTO_EXT="tar.gz"
 PLUTO_VERSION="0.11.4"
-PLUTO_URL_DIR="https://github.com/bondhugula/pluto/archive/refs/tags"
-PLUTO_URL_FILE="${PLUTO_VERSION}.${PLUTO_EXT}"
-PLUTO_URL="${PLUTO_URL_DIR}/${PLUTO_URL_FILE}"
 PLUTO_PREFIX="${OPT}/pluto-${PLUTO_VERSION}"
 
 PLUTO_CONFIGURE_ARGS=(
     --prefix=${PLUTO_PREFIX}
     --with-clang-prefix=${LLVM_PREFIX}
-    # --enable-debug
-    # --with-pet=bundled
-    # --with-isl=bundled
 )
 
-# wget -nc "${PLUTO_URL}" -O "${DOWNLOAD}/${PLUTO_FILE}.${PLUTO_EXT}"
-
 pushd ${BUILD}
-rm -fr pluto
 [ -e pluto ] || git clone --recurse-submodule git@github.com:bondhugula/pluto.git
-
 pushd pluto
 
-echo ">>> autogen.sh <<<"
-sh -x ./autogen.sh
-echo "<<< autogen.sh >>>"
-
-echo ">>> configure.sh <<<"
-sh -x ./configure "${PLUTO_CONFIGURE_ARGS[@]}"
-echo "<<< configure.sh >>>"
-
-echo ">>> make <<<"
-make -j V=1 SHELL="sh -x" LDFLAGS="-lclangFrontend -lclangBasic -lclangLex -lclangDriver"
-echo "<<< make >>>"
-
+./autogen.sh
+./configure "${PLUTO_CONFIGURE_ARGS[@]}"
+make -j LDFLAGS="-lclangFrontend -lclangBasic -lclangLex -lclangDriver"
 make -j test
 
 rm -rf "${PLUTO_PREFIX}"
