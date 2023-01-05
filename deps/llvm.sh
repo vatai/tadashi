7df9c0ddf5a416177f2a6b58af3df892e5f5c181
Emil VATAI <emil.vatai@gmail.com>
2023-01-05 09:37:19 +0900

    Combine `patches` dir with `downloads`

--- deps/llvm.sh
+++ deps/llvm.sh
@@ -20,14 +20,14 @@ LLVM_CMAKE_ARGS=(
 
 wget -nc "${LLVM_URL}" -O "${DOWNLOAD}/${LLVM_FILE}.${LLVM_EXT}" || true
 
-wget -nc "https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/llvm11/trunk/utils-benchmark-fix-missing-include.patch" -O "${PATCH}/utils-benchmark-fix-missing-include.patch" || true
+wget -nc "https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/llvm11/trunk/utils-benchmark-fix-missing-include.patch" -O "${DOWNLOAD}/utils-benchmark-fix-missing-include.patch" || true
 
 pushd "${BUILD}"
 [ -e "${LLVM_EXTRACTED}" ] || tar xvf "${DOWNLOAD}/${LLVM_FILE}.${LLVM_EXT}"
 pushd "${LLVM_EXTRACTED}"
 
 mkdir_ok_if_exists build
-patch -Np1 -i "${PATCH}/utils-benchmark-fix-missing-include.patch" || true
+patch -Np1 -i "${DOWNLOAD}/utils-benchmark-fix-missing-include.patch" || true
 # CC=clang CXX=clang++
 cmake -S llvm -B build "${LLVM_CMAKE_ARGS[@]}"
 cmake --build build
