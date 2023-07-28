fbcdf785f62e1920a938eb0940a8448b9edeae0a
Emil VATAI <emil.vatai@gmail.com>
2023-07-28 15:27:56 +0900

    Use ISL args properly

--- src/polegality.c
+++ src/polegality.c
@@ -1,8 +1,10 @@
 #include <assert.h>
+#include <isl/ast.h>
 #include <stdio.h>
 
 #include <isl/aff.h>
 #include <isl/aff_type.h>
+#include <isl/arg.h>
 #include <isl/ctx.h>
 #include <isl/flow.h>
 #include <isl/id.h>
@@ -24,40 +26,19 @@ struct options {
   struct isl_options *isl;
   struct pet_options *pet;
   char *schedule;
-  char *code;
+  char *source_file;
   unsigned tree;
 };
 
 ISL_ARGS_START(struct options, options_args)
 ISL_ARG_CHILD(struct options, isl, "isl", &isl_options_args, "isl options")
 ISL_ARG_CHILD(struct options, pet, NULL, &pet_options_args, "pet options")
-ISL_ARG_ARG(struct options, schedule, "schedule", NULL)
-ISL_ARG_ARG(struct options, code, "code", NULL)
-ISL_ARG_BOOL(struct options, tree, 0, "tree", 0,
-             "input schedule is specified as schedule tree")
+ISL_ARG_ARG(struct options, source_file, "source file", 0)
+ISL_ARG_STR(struct options, schedule, 's', "schedule", "schedule", 0,
+            "Yaml file describing the schedule")
 ISL_ARGS_END
 
 ISL_ARG_DEF(options, struct options, options_args)
-
-struct Args {
-  char *filename;
-  char *schedule;
-};
-
-struct Args get_args(int argc, char *argv[]) {
-  struct Args args;
-  if (argc < 2) {
-    printf("Usage: %s <C/C++ source file> <schedule>\n", argv[0]);
-    exit(-1);
-  }
-  args.filename = argv[1];
-
-  args.schedule = 0;
-  if (argc >= 3) {
-    args.schedule = argv[2];
-  }
-  return args;
-}
 
 __isl_give isl_union_flow *get_flow_from_scop(__isl_keep pet_scop *scop) {
   isl_union_map *sink, *may_source, *must_source;
@@ -138,13 +119,11 @@ void codegen(isl_ctx *ctx, char *schedule_str) {
 }
 
 int main(int argc, char *argv[]) {
-  struct Args args = get_args(argc, argv);
-  printf("Input file: %s\n", args.filename);
-  printf("Input schedule %s\n", args.schedule);
-
   struct options *options = options_new_with_defaults();
+  options_parse(options, argc, argv, ISL_ARG_ALL);
   isl_ctx *ctx = isl_ctx_alloc_with_options(&options_args, options);
-  pet_scop *scop = pet_scop_extract_from_C_source(ctx, args.filename, 0);
+
+  pet_scop *scop = pet_scop_extract_from_C_source(ctx, options->source_file, 0);
   if (!scop) {
     printf("No scop found!\n");
     isl_ctx_free(ctx);
@@ -152,10 +131,10 @@ int main(int argc, char *argv[]) {
   }
 
   isl_union_map *dependencies = get_dependencies(scop);
-  if (args.schedule) {
-    isl_bool legal = check_legality(ctx, args.schedule, dependencies);
-    print_legality(args.schedule, legal);
-    codegen(ctx, args.schedule);
+  if (options->schedule) {
+    isl_bool legal = check_legality(ctx, options->schedule, dependencies);
+    print_legality(options->schedule, legal);
+    codegen(ctx, options->schedule);
   } else {
     isl_schedule *schedule = pet_scop_get_schedule(scop);
     isl_union_map *schedule_map = isl_schedule_get_map(schedule);
