65b141de2b946616c3d2a1d45a06ac9f15ab679b
Emil VATAI <emil.vatai@gmail.com>
2023-08-04 17:22:32 +0900

    Update many.c

--- examples/many.c
+++ examples/many.c
@@ -2,61 +2,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void f(double *data, size_t M, size_t N) {
+void f(size_t M, size_t N, double data[M][N]) {
   // first scop: horizontal stencil
   for (size_t i = 0; i < M; i++) {
     for (size_t j = 1; j < N - 1; j++) {
-      size_t idx = i * N + j;
-      data[idx] = (data[idx - 1] + data[idx] + data[idx + 1]) / 3;
+      data[i][j] = (data[i - 1][j] + data[i][j] + data[i + 1][j]) / 3;
     }
   }
 
   // non-scop part;
-  int r = min(rand(), M * N);
+  int r = rand() % (N * M);
   for (size_t t = 1; t < r; t++) {
     // int r = rand();
+    size_t i = r / N, j = r % N;
     if (r == 0) {
-      data[t] = 0;
+      data[i][j] = 0;
     } else if (r % 2 == 0) {
-      data[t] += -data[t];
+      data[i][j] -= 0.5 * data[i][j];
     } else {
-      data[t] += 2 * data[t];
+      data[i][j] -= 1.5 * data[i][j];
     }
   }
 
   // second scop: vertical stencil
   for (size_t i = 1; i < M - 1; i++) {
     for (size_t j = 0; j < N; j++) {
-      size_t idx = i * N + j;
-      data[idx] = (data[idx - N] + data[idx] + data[idx + N]) / 3;
+      data[i][j] = (data[i][j - 1] + data[i][j] + data[i][j + 1]) / 3;
     }
   }
 }
 
-void g(double *data, size_t M, size_t N) {
+void g(size_t M, size_t N, double data[M][N]) {
   // third stencil
   for (size_t i = 1; i < M - 1; i++) {
-    for (size_t j = 0; j < N; j++) {
-      size_t idx = i * N + j;
-      data[idx] = (data[idx - N] + data[idx - 1] + data[idx] + data[idx + 1] +
-                   data[idx + N]) /
-                  5;
+    for (size_t j = 1; j < N - 1; j++) {
+      data[i][j] = (data[i][j - 1] + data[i - 1][j] + data[i][j] +
+                    data[i + 1][j] + data[i][j + 1]) /
+                   5;
     }
   }
 }
 
-void init(double *data, size_t M, size_t N) {
-  for (size_t i = 0; i < M * N; i++) {
-    data[i] = rand();
+void init(size_t M, size_t N, double data[M][N]) {
+  for (size_t i = 0; i < M; i++) {
+    for (size_t j = 0; j < N; j++) {
+      data[i][j] = rand();
+    }
   }
 }
 
-void print_matrix(double *data, size_t M, size_t N) {
+void print_matrix(size_t M, size_t N, double data[M][N]) {
 
   for (size_t i = 0; i < M; i++) {
     for (size_t j = 1; j < N - 1; j++) {
       size_t idx = i * N + j;
-      printf("%15.2lf ", data[idx]);
+      printf("%15.2lf ", data[i][j]);
     }
     printf("\n");
   }
@@ -64,15 +64,14 @@ void print_matrix(double *data, size_t M, size_t N) {
 
 int main(int argc, char *argv[]) {
   size_t M = 10, N = 10;
-  double *data = malloc(M * N * sizeof(double));
+  double data[M][N];
 
-  init(data, M, N);
-  print_matrix(data, M, N);
-  f(data, M, N);
-  g(data, M, N);
+  init(M, N, data);
+  print_matrix(M, N, data);
+  f(M, N, data);
+  g(M, N, data);
   printf("====\n");
-  print_matrix(data, M, N);
-  free(data);
+  print_matrix(M, N, data);
   printf("Done\n");
   return 0;
 }
