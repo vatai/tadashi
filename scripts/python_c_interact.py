1a779c4d0316254767fcbaf9fde3161e6a608e90
Emil VATAI <emil.vatai@gmail.com>
2023-08-21 01:27:01 +0900

    Update python_c_interact.py

--- scripts/python_c_interact.py
+++ scripts/python_c_interact.py
@@ -13,19 +13,23 @@ SCHED = '{ domain: "[N, i] -> { S_0[] }" }'
 child = pexpect.spawn(cmd[0])
 child.expect(".*domain.*")
 sched0 = child.after.rstrip().decode().split("\r\n")[-1]
+assert child.before == b""
 print(f"{sched0=}")
 
 child.sendline(sched0.replace("[(i)]", "[(-i)]"))
 
-child.expect("{ domain.*$")
-sched1 = child.after.rstrip().decode().split("\r\n")[-1]
+child.expect("sch.*\n{ domain.*")
+sched1 = child.after  # .rstrip().decode()  # .split("\r\n")[-1]
+assert child.before == b"", child.before
 print(f"{sched1=}")
 
-child.expect("{ domain.*$")
-sched3 = child.after.rstrip().decode().split("\r\n")[-1]
-print(f"{sched3=}")
-child.sendline(SCHED)
+# child.expect(".*{ domain.*")
+# sched1 = child.after  # .rstrip().decode()  # .split("\r\n")[-1]
+# assert child.before == b""
+# print(f"{sched1=}")
 
-child.expect("{ domain.*$")
-sched4 = child.after.rstrip().decode().split("\r\n")[-1]
-print(f"{sched4=}")
+# child.sendline(SCHED)
+
+# child.expect("{ domain.*$")
+# sched4 = child.after.rstrip().decode().split("\r\n")[-1]
+# print(f"{sched4=}")
