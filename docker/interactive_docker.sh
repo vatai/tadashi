6610fcb5350403e95d468958ae98176ebd296382
Emil VATAI <emil.vatai@gmail.com>
2023-09-18 13:33:19 +0900

    Save before switching to archlinux

--- docker/interactive_docker.sh
+++ docker/interactive_docker.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
-docker run --rm -it \
-       -v $(pwd):/workdir \
-       -w /workdir \
-       polytut:latest "$@"
-       # --env LD_LIBRARY_PATH=/barvinok/.libs:/barvinok/pet/.libs:/barvinok/isl/.libs \
-       # --env PYTHONPATH=/barvinok:/barvinok/pet/interface \
+DOCKER_ARGS=(
+    run --rm -it
+    # -v $(pwd):/workdir
+    -v $(pwd)/..:/workdir
+    # -w /workdir
+    -w /barvinok
+    polytut:latest "$@"
+    # --env LD_LIBRARY_PATH=/barvinok/.libs:/barvinok/pet/.libs:/barvinok/isl/.libs
+    # --env PYTHONPATH=/barvinok:/barvinok/pet/interface
+)
+docker ${DOCKER_ARGS[@]}
