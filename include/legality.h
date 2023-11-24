1ab23912a24361a380d474af2779d37f29a330a7
Emil VATAI <emil.vatai@gmail.com>
2023-11-24 21:52:20 +0900

    Move test code to Gtest

---
+++ include/legality.h
@@ -0,0 +1,17 @@
+#ifndef _LEGALITY_H_
+#define _LEGALITY_H_
+
+#include <isl/aff.h>
+#include <isl/set.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+isl_stat piece_lexpos(isl_set *set, isl_multi_aff *ma, void *user);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // _LEGALITY_H_
--- tests/legality_test.cc
+++ tests/legality_test.cc
@@ -1,7 +1,75 @@
+#include <string>
+#include <vector>
+
 #include <gtest/gtest.h>
 
+#include <isl/aff.h>
+#include <isl/ctx.h>
+#include <isl/set.h>
+
+#include "../include/legality.h"
+
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(LegalityTest, BasicAssertions) {
+  struct test_data_t {
+    std::string input;
+    int output;
+  };
+
+  isl_ctx *ctx = isl_ctx_alloc();
+  std::vector<struct test_data_t> data = {
+      {"{ [ i ] -> [ -1 ]  }", -1}, //
+      {"{ [ i ] -> [ 0 ]  }", 0},   //
+      {"{ [ i ] -> [ 1 ]  }", 1},   //
+      //
+      {"{ [ i ] -> [ -1, -1 ]  }", -1}, //
+      {"{ [ i ] -> [ -1, 0 ]  }", -1},  //
+      {"{ [ i ] -> [ -1, 1 ]  }", -1},  //
+      {"{ [ i ] -> [ 0, -1 ]  }", -1},  //
+      {"{ [ i ] -> [ 0, 0 ]  }", 0},    //
+      {"{ [ i ] -> [ 0, 1 ]  }", 1},    //
+      {"{ [ i ] -> [ 1, -1 ]  }", 1},   //
+      {"{ [ i ] -> [ 1, 0 ]  }", 1},    //
+      {"{ [ i ] -> [ 1, 1 ]  }", 1},    //
+      //
+      {"{ [ i ] -> [ -1, -1, -1 ]  }", -1}, //
+      {"{ [ i ] -> [ -1, -1, 0 ]  }", -1},  //
+      {"{ [ i ] -> [ -1, -1, 1 ]  }", -1},  //
+      {"{ [ i ] -> [ -1, 0, -1 ]  }", -1},  //
+      {"{ [ i ] -> [ -1, 0, 0 ]  }", -1},   //
+      {"{ [ i ] -> [ -1, 0, 1 ]  }", -1},   //
+      {"{ [ i ] -> [ -1, 1, -1 ]  }", -1},  //
+      {"{ [ i ] -> [ -1, 1, 0 ]  }", -1},   //
+      {"{ [ i ] -> [ -1, 1, 1 ]  }", -1},   //
+      {"{ [ i ] -> [ 0, -1, -1 ]  }", -1},  //
+      {"{ [ i ] -> [ 0, -1, 0 ]  }", -1},   //
+      {"{ [ i ] -> [ 0, -1, 1 ]  }", -1},   //
+      {"{ [ i ] -> [ 0, 0, -1 ]  }", -1},   //
+      {"{ [ i ] -> [ 0, 0, 0 ]  }", 0},     //
+      {"{ [ i ] -> [ 0, 0, 1 ]  }", 1},     //
+      {"{ [ i ] -> [ 0, 1, -1 ]  }", 1},    //
+      {"{ [ i ] -> [ 0, 1, 0 ]  }", 1},     //
+      {"{ [ i ] -> [ 0, 1, 1 ]  }", 1},     //
+      {"{ [ i ] -> [ 1, -1, -1 ]  }", 1},   //
+      {"{ [ i ] -> [ 1, -1, 0 ]  }", 1},    //
+      {"{ [ i ] -> [ 1, -1, 1 ]  }", 1},    //
+      {"{ [ i ] -> [ 1, 0, -1 ]  }", 1},    //
+      {"{ [ i ] -> [ 1, 0, 0 ]  }", 1},     //
+      {"{ [ i ] -> [ 1, 0, 1 ]  }", 1},     //
+      {"{ [ i ] -> [ 1, 1, -1 ]  }", 1},    //
+      {"{ [ i ] -> [ 1, 1, 0 ]  }", 1},     //
+      {"{ [ i ] -> [ 1, 1, 1 ]  }", 1},     //
+  };
+  size_t nbtests = sizeof(data) / sizeof(data[0]);
+  for (size_t i = 0; i < nbtests; i++) {
+    isl_multi_aff *ma = isl_multi_aff_read_from_str(ctx, data[i].input.c_str());
+    isl_set *set = isl_set_read_from_str(ctx, "{ : }");
+    int rv;
+    isl_stat stat = piece_lexpos(set, ma, &rv);
+    assert(stat == isl_stat_ok);
+    printf("test %lu: %s\n", i,
+           rv == data[i].output ? "OK" : data[i].input.c_str());
+  }
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
