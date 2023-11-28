faf77726e8baa6e2560860e07f1c298def4c3d89
Emil VATAI <emil.vatai@gmail.com>
2023-11-29 06:20:33 +0900

    Add missing codegen.h

---
+++ include/codegen.h
@@ -0,0 +1,19 @@
+#ifndef _CODEGEN_H_
+#define _CODEGEN_H_
+
+#include <isl/schedule.h>
+#include <pet.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+__isl_give isl_printer *generate_code(isl_ctx *ctx, __isl_take isl_printer *p,
+                                      struct pet_scop *scop,
+                                      isl_schedule *schedule);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // _CODEGEN_H_
