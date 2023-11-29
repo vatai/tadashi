add47c5f0ba75f62ed003e6ba0e2700f4d7b2381
Emil VATAI <emil.vatai@gmail.com>
2023-11-29 22:35:12 +0900

    Update eval polybench

--- scripts/eval-polybench.sh
+++ scripts/eval-polybench.sh
@@ -1,8 +1,8 @@
 !/usr/bin/bash
 
 source "$(dirname $0)/common.sh"
-CMD="$GIT_ROOT/build/polegality"
-POLYROOT=$GIT_ROOT/deps/downloads/polybench-c-3.2
+CMD="$GIT_ROOT/build/tadashi --no-legality-check -d tadashi.isl_union_map -s tadashi.yaml"
+POLYROOT=$GIT_ROOT/deps/polybench-c-3.2
 POLYFILES="$(find $POLYROOT -mindepth 3 -name '*.c')"
 for BENCHMARK_FILE in ${POLYFILES}; do
     export C_INCLUDE_PATH="${POLYROOT}/utilities"
