8c61970ea6e16a6c9592e6d7699ac435e7141cc9
Emil VATAI <emil.vatai@gmail.com>
2023-12-06 21:23:00 +0900

    Find sink example

--- src/scratch.c
+++ src/scratch.c
@@ -34,7 +34,7 @@ int main() {
   fclose(file);
 
   isl_schedule_node *node = isl_schedule_get_root(schedule);
-  isl_schedule_node_dump(node);
+  // isl_schedule_node_dump(node);
   node = isl_schedule_node_first_child(node);
   assert(isl_schedule_node_get_type(node) == isl_schedule_node_sequence);
 
@@ -66,15 +66,15 @@ int main() {
   node = isl_schedule_node_cut(node);
   node = isl_schedule_node_parent(node);
 
-  isl_schedule_node_dump(node);
+  // isl_schedule_node_dump(node);
 
   // node = isl_schedule_node_insert_filter(node, f);
   isl_union_set_list *flist = isl_union_set_list_from_union_set(f0);
   flist = isl_union_set_list_insert(flist, 1, f1);
 
   isl_schedule_node_free(subtree);
-  isl_schedule_node_dump(node);
-  isl_multi_union_pw_aff_dump(p1);
+  // isl_schedule_node_dump(node);
+  // isl_multi_union_pw_aff_dump(p1);
   isl_union_set_free(f);
   isl_union_set_list_free(flist);
 
@@ -83,6 +83,77 @@ int main() {
   isl_schedule_node_free(node);
   isl_schedule_free(schedule);
   isl_union_map_free(umap);
+
+  schedule = isl_schedule_from_domain(
+      isl_union_set_read_from_str(ctx, "[N] -> {S_0[i] : 0 <= i < N;"         //
+                                       "S_1[i, j] : 0 < i < N and 0 < j < N;" //
+                                       "S_2[i, j] : 0 < i < N and 0 < j < N; "
+                                       "S_3[i] : 0 <= i < N }"));
+  node = isl_schedule_get_root(schedule);
+
+  node = isl_schedule_node_first_child(node);
+  flist = isl_union_set_list_from_union_set(
+      isl_union_set_read_from_str(ctx, "[N] -> { S_0[i] }"));
+  flist = isl_union_set_list_insert(
+      flist, isl_union_set_list_size(flist),
+      isl_union_set_read_from_str(ctx, "[N]->{S_1[i,j]; S_2[i,j] }"));
+  flist = isl_union_set_list_insert(
+      flist, isl_union_set_list_size(flist),
+      isl_union_set_read_from_str(ctx, "[N]->{S_3[i,j] }"));
+  node = isl_schedule_node_insert_sequence(node, flist);
+
+  node = isl_schedule_node_child(node, 0);
+  node = isl_schedule_node_first_child(node);
+  node = isl_schedule_node_insert_partial_schedule(
+      node, isl_multi_union_pw_aff_read_from_str(
+                ctx, "[N] -> L_0[{ S_0[i] -> [i]}]"));
+
+  node = isl_schedule_node_first_child(node);
+  node = isl_schedule_node_insert_mark(node, isl_id_read_from_str(ctx, "foo"));
+  node = isl_schedule_node_parent(node);
+
+  node = isl_schedule_node_parent(node);
+
+  node = isl_schedule_node_next_sibling(node);
+  node = isl_schedule_node_first_child(node);
+  node = isl_schedule_node_insert_partial_schedule(
+      node, isl_multi_union_pw_aff_read_from_str(
+                ctx, "[N] -> L_1[{ S_1[i,j] -> [i]; S_2[i,j] -> [i] }]"));
+  node = isl_schedule_node_first_child(node);
+  node = isl_schedule_node_insert_partial_schedule(
+      node, isl_multi_union_pw_aff_read_from_str(
+                ctx, "[N] -> L_2[{ S_1[i,j] -> [j]; S_2[i,j] -> [j] }]"));
+  flist = isl_union_set_list_from_union_set(
+      isl_union_set_read_from_str(ctx, "[N] -> {S_1[i,j]}"));
+  flist = isl_union_set_list_insert(
+      flist, isl_union_set_list_size(flist),
+      isl_union_set_read_from_str(ctx, "[N] -> {S_2[i,j]}"));
+  node = isl_schedule_node_first_child(node);
+  node = isl_schedule_node_insert_sequence(node, flist);
+  node = isl_schedule_node_parent(node);
+  node = isl_schedule_node_band_sink(node); // Try commenting this
+  node = isl_schedule_node_parent(node);
+  node = isl_schedule_node_parent(node);
+
+  node = isl_schedule_node_next_sibling(node);
+  node = isl_schedule_node_first_child(node);
+  node = isl_schedule_node_insert_partial_schedule(
+      node, isl_multi_union_pw_aff_read_from_str(
+                ctx, "[N] -> L_3[{ S_3[i] -> [i]}]"));
+
+  node = isl_schedule_node_first_child(node);
+  node = isl_schedule_node_order_before(
+      node, isl_union_set_read_from_str(
+                ctx, "[N]->{ S_3[i] : 0 <= i < floor(N/2) }"));
+  // node = isl_schedule_node_parent(node);
+  // node = isl_schedule_node_parent(node);
+  // node = isl_schedule_node_first_child(node);
+  // node = isl_schedule_node_parent(node);
+  // node = isl_schedule_node_parent(node);
+
+  isl_schedule_node_dump(node);
+  isl_schedule_node_free(node);
+  isl_schedule_free(schedule);
 
   isl_ctx_free(ctx);
   printf("Bye!\n");
