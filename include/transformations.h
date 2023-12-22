6406850435b1e8a0fd95202b6e2c93f39a5a2e3a
Emil VATAI <emil.vatai@gmail.com>
2023-12-22 11:33:40 +0900

    Add `transformations.{c,h}`

---
+++ include/transformations.h
@@ -0,0 +1,15 @@
+#ifndef _TRANSFORMATIONS_H_
+#define _TRANSFORMATIONS_H_
+
+#include <isl/schedule.h>
+#include <pet.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // _TRANSFORMATIONS_H_
