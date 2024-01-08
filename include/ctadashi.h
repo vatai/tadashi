3ee73d3b2cdda1ffc010b65bc77f01d00ea71f66
Emil VATAI <emil.vatai@gmail.com>
2024-01-09 02:16:28 +0900

    Start writing cython code

---
+++ include/ctadashi.h
@@ -0,0 +1,14 @@
+#ifndef _CTADASHI_H_
+#define _CTADASHI_H_
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+// struct cell *foo(int n);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // _CTADASHI_H_
--- tadashi/tadashi_interface.pyx
+++ tadashi/tadashi_interface.pyx
@@ -1,2 +1,7 @@
-def hello():
-    print("Hello")
+cdef extern from "../src/ctadashi.cc":
+    pass
+
+# Declare the class with cdef
+cdef extern from "../include/ctadashi.h"
+    cdef cppclass Rectangle:
+        Rectangle() except +
