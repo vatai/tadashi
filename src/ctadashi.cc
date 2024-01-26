eeebadae9b7b5a1c94b5ce38f6dbfc5117e78d00
Emil VATAI <emil.vatai@gmail.com>
2024-01-26 11:53:53 +0900

    Clean up ctadashi.cc

--- src/ctadashi.cc
+++ src/ctadashi.cc
@@ -20,68 +20,7 @@
 #include <string>
 #include <vector>
 
-struct node_t {
-  size_t parent;
-  size_t id;
-};
-
-std::vector<node_t> NODES;
-
-typedef std::pair<isl_size, isl_size> depth_position_t;
-typedef std::map<depth_position_t, size_t> map_deppos2idx_t;
-map_deppos2idx_t DEPPOS2IDX;
-
 extern "C" {
-
-depth_position_t _get_depth_position(isl_schedule_node *node) {
-  isl_size pos = 0;
-  if (isl_schedule_node_has_parent(node))
-    pos = isl_schedule_node_get_child_position(node);
-  isl_size dep = isl_schedule_node_get_tree_depth(node);
-  return {dep, pos};
-}
-
-isl_bool foreach_node(isl_schedule_node *node, void *user) {
-  size_t id = NODES.size();
-  depth_position_t dp = _get_depth_position(node);
-  DEPPOS2IDX[dp] = id;
-  printf("DEPPOS2IDX[%d, %d] = %ld\n", dp.first, dp.second, id);
-  if (!isl_schedule_node_has_parent(node)) {
-    NODES.push_back({.parent = -1lu, .id = id});
-    printf("add(parent=-1, id=%lu)\n", id);
-    printf("---\n");
-    return isl_bool_true;
-  }
-  node = isl_schedule_node_parent(node); // go to parent
-  map_deppos2idx_t::iterator parent;
-  parent = DEPPOS2IDX.find(_get_depth_position(node));
-  assert(parent != DEPPOS2IDX.end());
-  node = isl_schedule_node_child(node, dp.second); // return from parent
-  NODES.push_back({.parent = parent->second, .id = id});
-  printf("add(parent=%lu, id=%lu)\n", parent->second, id);
-  printf("---\n");
-  return isl_bool_true;
-}
-
-__isl_give isl_printer *foreach_scope(__isl_take isl_printer *p, pet_scop *scop,
-                                      void *user) {
-  isl_schedule *sched = pet_scop_get_schedule(scop);
-  isl_schedule_foreach_schedule_node_top_down(sched, foreach_node, user);
-  isl_schedule_free(sched);
-  pet_scop_free(scop);
-  return p;
-}
-
-int scan_source(char *input) { // Entry point
-  isl_ctx *ctx = isl_ctx_alloc_with_pet_options();
-  FILE *output = fopen("cout.c", "w");
-
-  pet_transform_C_source(ctx, input, output, foreach_scope, NULL);
-
-  fclose(output);
-  isl_ctx_free(ctx);
-  return 0;
-}
 
 std::vector<pet_scop *> SCOPS;
 std::vector<isl_schedule_node *> ROOTS;
@@ -119,13 +58,13 @@ void free_scops() {
   isl_ctx_free(ctx);
 }
 
-int get_type(size_t node_idx) {
-  return isl_schedule_node_get_type(ROOTS[node_idx]);
+int get_type(size_t scop_idx) {
+  return isl_schedule_node_get_type(ROOTS[scop_idx]);
 }
 
-const char *get_type_str(size_t node_idx) {
+const char *get_type_str(size_t scop_idx) {
   enum isl_schedule_node_type type;
-  type = isl_schedule_node_get_type(ROOTS[node_idx]);
+  type = isl_schedule_node_get_type(ROOTS[scop_idx]);
   const char *type2str[11];
 
   type2str[isl_schedule_node_band] = "BND";
@@ -146,20 +85,20 @@ size_t get_num_children(size_t scop_idx) {
   return isl_schedule_node_n_children(ROOTS[scop_idx]);
 }
 
-size_t depth(size_t node_idx) {
-  return isl_schedule_node_get_tree_depth(ROOTS[node_idx]);
+size_t depth(size_t scop_idx) {
+  return isl_schedule_node_get_tree_depth(ROOTS[scop_idx]);
 }
 
-size_t child_position(size_t node_idx) {
-  return isl_schedule_node_get_child_position(ROOTS[node_idx]);
+size_t child_position(size_t scop_idx) {
+  return isl_schedule_node_get_child_position(ROOTS[scop_idx]);
 }
 
-void goto_parent(size_t node_idx) {
-  ROOTS[node_idx] = isl_schedule_node_parent(ROOTS[node_idx]);
+void goto_parent(size_t scop_idx) {
+  ROOTS[scop_idx] = isl_schedule_node_parent(ROOTS[scop_idx]);
 }
 
 void goto_child(size_t scop_idx, size_t child_idx) {
-  ROOTS[scop_idx] = isl_schedule_node_child(ROOTS[node_idx], child_idx);
+  ROOTS[scop_idx] = isl_schedule_node_child(ROOTS[scop_idx], child_idx);
 }
 
 const char *get_expr(size_t idx) {
@@ -172,8 +111,8 @@ const char *get_expr(size_t idx) {
   return "read MUPA line\n";
 }
 
-const char *get_dim_names(size_t idx) {
-  isl_schedule_node *node = ROOTS[idx];
+const char *get_dim_names(size_t scop_idx) {
+  isl_schedule_node *node = ROOTS[scop_idx];
   if (isl_schedule_node_get_type(node) != isl_schedule_node_band)
     return "";
   std::stringstream ss;
@@ -200,44 +139,11 @@ const char *get_dim_names(size_t idx) {
   return ss.str().c_str();
 }
 
-const char *get_schedule_yaml(size_t idx) {
-  isl_schedule *sched = pet_scop_get_schedule(SCOPS[idx]);
+const char *get_schedule_yaml(size_t scop_idx) {
+  isl_schedule *sched = pet_scop_get_schedule(SCOPS[scop_idx]);
   const char *ptr = isl_schedule_to_str(sched);
   isl_schedule_free(sched);
   return ptr;
 }
 
 } // extern "C"
-
-/***** remove BEGIN *****/
-struct cell {
-  unsigned long long name;
-  struct cell *next;
-};
-
-extern "C" struct cell *foo(int n) {
-  struct cell *t, *first;
-  first = t = (struct cell *)malloc(sizeof(struct cell));
-  for (size_t i = 0; i < n; ++i) {
-    t->name = i;
-    t->next = (struct cell *)malloc(sizeof(struct cell));
-    t = t->next;
-  }
-  t->name = n;
-  t->next = 0;
-  return first;
-}
-
-extern "C" size_t bar(struct cell *t) {
-  struct cell *next;
-  size_t count = 0;
-  while (t) {
-    next = t->next;
-    free(t);
-    count++;
-    t = next;
-  }
-  return count;
-}
-
-/***** remove END *****/
