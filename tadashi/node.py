e342e6ee7335b4ed22aef3ded5aec23cba61652e
Aleksandr Drozd <alexander.drozd@gmail.com>
2024-01-26 12:13:12 +0900

    create scop class

--- tadashi/node.py
+++ tadashi/node.py
@@ -1,19 +1,40 @@
 from core import *
 
 
-def get_node(scop_idx, parent):
-    num_children = get_num_children(scop_idx)
-    inner_dim_names = get_dim_names(scop_idx).decode().split(";")[:-1]
-    dim_names = [t.split("|")[:-1] for t in inner_dim_names]
-    node = Node(
-        node_type=get_type(scop_idx),
-        type_str=get_type_str(scop_idx),
-        num_children=num_children,
-        parent=parent,
-        dim_names=dim_names,
-        expr=get_expr(scop_idx),
-    )
-    return node
+class Scop:
+    def __init__(self, idx) -> None:
+        self.idx = idx
+
+    def get_current_node_from_ISL(self, parent):
+        num_children = get_num_children(self.idx)
+        inner_dim_names = get_dim_names(self.idx).decode().split(";")[:-1]
+        dim_names = [t.split("|")[:-1] for t in inner_dim_names]
+        node = Node(
+            node_type=get_type(self.idx),
+            type_str=get_type_str(self.idx),
+            num_children=num_children,
+            parent=parent,
+            dim_names=dim_names,
+            expr=get_expr(self.idx),
+        )
+        return node
+
+    def traverse(self, nodes, parent):
+        node = self.get_current_node_from_ISL(parent)
+        print(f"{node=}")
+        parent_idx = len(nodes)
+        nodes.append(node)
+        if not node.is_leaf():
+            for c in range(node.num_children):
+                goto_child(self.idx, c)
+                node.children[c] = len(nodes)
+                self.traverse(nodes, parent_idx)
+                goto_parent(self.idx)
+
+    def get_schedule_tree(self):
+        nodes = []
+        self.traverse(nodes, parent=-1)
+        return nodes
 
 
 class Node:
--- tadashi/tadashilib.py
+++ tadashi/tadashilib.py
@@ -2,27 +2,8 @@
 
 import yaml
 
-from core import *
-from node import Node, get_node
-
-
-def traverse(scop_idx, nodes, parent):
-    node = get_node(scop_idx, parent)
-    print(f"{node=}")
-    parent_idx = len(nodes)
-    nodes.append(node)
-    if not node.is_leaf():
-        for c in range(node.num_children):
-            goto_child(scop_idx, c)
-            node.children[c] = len(nodes)
-            traverse(scop_idx, nodes, parent_idx)
-            goto_parent(scop_idx)
-
-
-def get_schedule_tree(scop_idx):
-    nodes = []
-    traverse(scop_idx, nodes, parent=-1)
-    return nodes
+from core import free_scops, get_num_scops
+from node import Node, Scop
 
 
 def compare(sch_tree, sched):
@@ -32,17 +13,15 @@ def compare(sch_tree, sched):
 def main():
     # TODO: Emil double check if something usefull was done here and remove
     # scan_source(b"./examples/depnodep.c")
-    num_scopes = get_num_scops(b"./examples/depnodep.c")
-    print(f"{num_scopes=}")
+    scops = [Scop(i) for i in range(get_num_scops(b"./examples/depnodep.c"))]
+    # print(f"{num_scopes=}")
     # child(0, 0)
-    print(f"{get_num_children(0)=}")
-    print(f"{get_dim_names(0)=}")
     schedules_trees = []
-    for i in range(num_scopes):
-        sched = get_schedule_yaml(i).decode()
-        sched = yaml.load(sched, Loader=yaml.SafeLoader)
-        sch_tree = get_schedule_tree(i)
-        compare(sch_tree, sched)
+    for scop in scops:
+        # sched = get_schedule_yaml(i).decode()
+        # sched = yaml.load(sched, Loader=yaml.SafeLoader)
+        sch_tree = scop.get_schedule_tree()
+        # compare(sch_tree, sched)
         schedules_trees.append(sch_tree)
     free_scops()
     print("PYTHON DONE")
