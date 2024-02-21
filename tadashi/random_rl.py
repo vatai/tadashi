76117c9ae6369899ceafecc4fd98b6e3021fd565
Emil VATAI <emil.vatai@gmail.com>
2024-02-21 17:22:29 +0900

    Rename files

---
+++ tadashi/random_rl.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from benchmarks import Polybench
+from tadashilib import Scops
+
+# import yaml
+
+
+def compare(sch_tree, sched):
+    """Compare the schedule tree with the ISL traversal in C."""
+    print("ok")
+    raise NotImplemented()
+
+
+def main():
+    pb_2mm = Polybench(
+        benchmark="linear-algebra/kernels/2mm",
+        base="./deps/downloads/polybench-c-3.2",
+    )
+    input_path = "./examples/depnodep.c"
+    output_path = "./examples/depnodep.tadashilib.c"
+    scops = Scops(pb_2mm)
+    schedules_trees = []
+    for scop in scops:
+        # sched = get_schedule_yaml(i).decode()
+        # sched = yaml.load(sched, Loader=yaml.SafeLoader)
+        sch_tree = scop.get_schedule_tree()
+        # compare(sch_tree, sched)
+        schedules_trees.append(sch_tree)
+
+    scop = schedules_trees[0]  # select_scop()
+    node = scop[1]  # model.select_node(scop)
+    print(f"{node=}")
+    # for each node, extract representation,
+    # get score for how promising it looks to be transformed
+    # select node to transform accroding to scores
+    # transform the node
+    # codegen new scops and measure performance
+    node.tile(10)
+    scops.generate_code(output_path)
+    print(f"PYTHON DONE")
+
+
+if __name__ == "__main__":
+    main()
--- tadashi/tadashilib.py
+++ tadashi/tadashilib.py
@@ -1,45 +1,157 @@
-#!/usr/bin/env python
+import os
+from ctypes import CDLL, c_char_p, c_int, c_size_t
+from pathlib import Path
 
-from benchmarks import Polybench
-from node import Scops
+from benchmarks import App
 
-# import yaml
-
-
-def compare(sch_tree, sched):
-    """Compare the schedule tree with the ISL traversal in C."""
-    print("ok")
-    raise NotImplemented()
+# from core import *
 
 
-def main():
-    pb_2mm = Polybench(
-        benchmark="linear-algebra/kernels/2mm",
-        base="./deps/downloads/polybench-c-3.2",
-    )
-    input_path = "./examples/depnodep.c"
-    output_path = "./examples/depnodep.tadashilib.c"
-    scops = Scops(pb_2mm)
-    schedules_trees = []
-    for scop in scops:
-        # sched = get_schedule_yaml(i).decode()
-        # sched = yaml.load(sched, Loader=yaml.SafeLoader)
-        sch_tree = scop.get_schedule_tree()
-        # compare(sch_tree, sched)
-        schedules_trees.append(sch_tree)
+class Scop:
+    """Single SCoP.
 
-    scop = schedules_trees[0]  # select_scop()
-    node = scop[1]  # model.select_node(scop)
-    print(f"{node=}")
-    # for each node, extract representation,
-    # get score for how promising it looks to be transformed
-    # select node to transform accroding to scores
-    # transform the node
-    # codegen new scops and measure performance
-    node.tile(10)
-    scops.generate_code(output_path)
-    print(f"PYTHON DONE")
+    In the .so file, there is a global `std::vecto` of `isl_scop`
+    objects.  Objects of `Scop` (in python) represents a the
+    `isl_scop` object by storing its index in the `std::vecto`.
+
+    """
+
+    def __init__(self, idx, ctadashi) -> None:
+        self.ctadashi = ctadashi
+        self.idx = idx
+
+    def read_dim_names(self):
+        # ctadashi return one string for the `dim_names` but there are
+        # two layers which need unpacking.
+        all_dim_names = self.ctadashi.get_dim_names(self.idx).decode()
+        outer_dim_names = all_dim_names.split(";")[:-1]
+        return [t.split("|")[:-1] for t in outer_dim_names]
+
+    def make_node(self, parent, location):
+        num_children = self.ctadashi.get_num_children(self.idx)
+        node = Node(
+            scop=self,
+            node_type=self.ctadashi.get_type(self.idx),
+            type_str=self.ctadashi.get_type_str(self.idx).decode("utf-8"),
+            num_children=num_children,
+            parent=parent,
+            location=location,
+            dim_names=self.read_dim_names(),
+            expr=self.ctadashi.get_expr(self.idx).decode("utf-8"),
+        )
+        return node
+
+    def traverse(self, nodes, parent, path):
+        node = self.make_node(parent, path)
+        print(f"{node=}")
+        current_idx = len(nodes)
+        nodes.append(node)
+        if not node.is_leaf():
+            for c in range(node.num_children):
+                self.ctadashi.goto_child(self.idx, c)
+                node.children[c] = len(nodes)
+                self.traverse(nodes, current_idx, path + [c])
+                self.ctadashi.goto_parent(self.idx)
+
+    def get_schedule_tree(self):
+        self.ctadashi.reset_root(self.idx)
+        nodes = []
+        self.traverse(nodes, parent=-1, path=[])
+        return nodes
+
+    def locate(self, location):
+        self.ctadashi.reset_root(self.idx)
+        for c in location:
+            self.ctadashi.goto_child(self.idx, c)
+
+    def tile(self, tile_size):
+        self.ctadashi.tile(self.idx, tile_size)
 
 
-if __name__ == "__main__":
-    main()
+class Scops:
+    """All SCoPs which belong to a given file.
+
+    The object of type `Scops` is similar to a list."""
+
+    def __init__(self, app: App):
+        self.setup_ctadashi(app)
+        self.app = app
+        self.source_path_bytes = str(self.app.source_path).encode()
+        print(f"{self.source_path_bytes=}")
+        self.num_scops = self.ctadashi.get_num_scops(self.source_path_bytes)
+        self.scops = [Scop(i, self.ctadashi) for i in range(self.num_scops)]
+
+    def setup_ctadashi(self, app: App):
+        os.environ["C_INCLUDE_PATH"] = ":".join(app.include_paths)
+        self.so_path = Path(__file__).parent.parent / "build/libctadashi.so"
+        self.ctadashi = CDLL(str(self.so_path))
+        self.ctadashi.get_num_scops.argtypes = [c_char_p]
+        self.ctadashi.get_num_scops.restype = c_int
+        self.ctadashi.get_type.argtypes = [c_size_t]
+        self.ctadashi.get_type.restype = c_int
+        self.ctadashi.get_type_str.argtypes = [c_size_t]
+        self.ctadashi.get_type_str.restype = c_char_p
+        self.ctadashi.get_num_children.argtypes = [c_size_t]
+        self.ctadashi.get_num_children.restype = c_size_t
+        self.ctadashi.goto_parent.argtypes = [c_size_t]
+        self.ctadashi.goto_child.argtypes = [c_size_t, c_size_t]
+        self.ctadashi.get_expr.argtypes = [c_size_t]
+        self.ctadashi.get_expr.restype = c_char_p
+        self.ctadashi.get_dim_names.argtypes = [c_size_t]
+        self.ctadashi.get_dim_names.restype = c_char_p
+        self.ctadashi.get_schedule_yaml.argtypes = [c_size_t]
+        self.ctadashi.get_schedule_yaml.restype = c_char_p
+        self.ctadashi.reset_root.argtypes = [c_size_t]
+        self.ctadashi.tile.argtypes = [c_size_t, c_size_t]
+        self.ctadashi.generate_code.argtypes = [c_char_p, c_char_p]
+
+    def generate_code(self, output_path):
+        self.ctadashi.generate_code(self.source_path_bytes, output_path.encode())
+
+    def __len__(self):
+        return self.num_scops
+
+    def __getitem__(self, idx):
+        return self.scops[idx]
+
+    def __del__(self):
+        self.ctadashi.free_scops()
+
+
+class Node:
+    LEAF_TYPE = 6
+
+    def __init__(
+        self,
+        scop,
+        node_type,
+        type_str,
+        num_children,
+        parent,
+        location,
+        dim_names,
+        expr,
+    ) -> None:
+        self.scop = scop
+        self.node_type = node_type
+        self.type_str = type_str
+        self.num_children = num_children
+        self.parent = parent
+        self.dim_names = dim_names
+        self.expr = expr
+        self.children = [-1] * num_children
+        self.location = location
+
+    def __repr__(self):
+        return f"Node type: {self.type_str}({self.node_type}), {self.dim_names}, {self.expr}, {self.location}"
+
+    def is_leaf(self):
+        return self.node_type == self.LEAF_TYPE
+
+    def locate(self):
+        self.scop.locate(self.location)
+        return self.scop.get_current_node_from_ISL(None, None)
+
+    def tile(self, tile_size):
+        self.scop.locate(self.location)
+        self.scop.tile(tile_size)
