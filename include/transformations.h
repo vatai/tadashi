c0844888195059bbebc9c79aed5f0019a533cfd9
Emil VATAI <emil.vatai@gmail.com>
2024-03-11 23:25:14 +0900

    Fix compilation

--- include/transformations.h
+++ include/transformations.h
@@ -19,9 +19,9 @@ isl_schedule_node *tadashi_tile_1d(isl_schedule_node *node, int si);
 
 isl_schedule_node *tadashi_interchange(isl_schedule_node *node);
 
-isl_schedule_node *tadashi_scale(isl_schedule_node *node);
+isl_schedule_node *tadashi_scale(isl_schedule_node *node, long scale);
 
-isl_schedule_node *tadashi_shift(isl_schedule_node *node);
+isl_schedule_node *tadashi_shift(isl_schedule_node *node, long shift);
 
 #if defined(__cplusplus)
 }
