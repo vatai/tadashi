843f3298e91a66b6a50c6e695749ac55f3fc37e8
Emil VATAI <emil.vatai@gmail.com>
2024-05-10 11:30:23 +0900

    Update random_rl.py

--- tadashi/random_rl.py
+++ tadashi/random_rl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from apps import Polybench
+from apps import Polybench, Simple
 from tadashilib import Scops, Transformation
 
 
@@ -9,9 +9,13 @@ def main():
         benchmark="linear-algebra/kernels/2mm",
         base="build/_deps/polybench-src/",
     )
+    node_idx = 5
+    # app = Simple(source="examples/depnodep-stripped.c")
+    app = Simple(source="examples/threeloop.c")
+    node_idx = 1
     scops = Scops(app)
     scop = scops[0]  # select_scop()
-    node = scop.schedule_tree[5]  # model.select_node(scop)
+    node = scop.schedule_tree[node_idx]  # model.select_node(scop)
     # print(f"{node=}")
     # for each node, extract representation,
     # get score for how promising it looks to be transformed
@@ -20,10 +24,10 @@ def main():
     # codegen new scops and measure performance
     print(f"{node.avaliable_transformation=}")
     # node.transform(Transformation.TILE, 10)
-    node.transform(Transformation.PARTIAL_SHIFT_ID, 0, 0)
+    node.transform(Transformation.PARTIAL_SHIFT_VAL, 0, 42)
     scops.generate_code()
-    app.compile()
-    print(f"{app.output_binary=}")
+    # app.compile()
+    # print(f"{app.output_binary=}")
     # app_time = app.measure()
     # print(f"{app_time=}")
 
