420074cffef8a63a4eae72306661e114d89f06b6
Emil VATAI <emil.vatai@gmail.com>
2024-05-27 13:37:58 +0900

    Move random_rl.py

--- tadashi/__main__.py
+++ tadashi/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from apps import Polybench, Simple
-from tadashilib import Scops, Transformation
+from .apps import Polybench, Simple
+from .tadashilib import Scops, Transformation
 
 
 def main():
