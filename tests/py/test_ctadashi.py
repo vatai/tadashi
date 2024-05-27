0e0aa59fa8bbe7c2961d483e4dcdccbc3d248245
Emil VATAI <emil.vatai@gmail.com>
2024-05-27 11:48:44 +0900

    Add python tests

---
+++ tests/py/test_ctadashi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import unittest
+
+
+class TestCtadashi(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    print("Hello")
