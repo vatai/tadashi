81b65f847f173e1f0eb2b90ecaee29cd7468f3d4
Emil VATAI <emil.vatai@gmail.com>
2024-05-31 12:27:28 +0900

    Add `scop_idx` and `node_idx` support for lit

--- tests/py/test_ctadashi.py
+++ tests/py/test_ctadashi.py
@@ -23,62 +23,43 @@ class TransformData:
     transformation_args: list[int] = field(default_factory=list)
 
 
-def foobar(app):
-    target_code = []
-    with open(app.source) as file:
-        for line in file:
-            if line.startswith(HEADER):
-                transform_str = line.replace(HEADER, "")
-            elif line.startswith(COMMENT):
-                stripped_line = line.strip().replace(COMMENT, "")
-                if len(line) > len(COMMENT):
-                    stripped_line = stripped_line[1:]
-                target_code.append(stripped_line)
-    transform = list(ast.literal_eval(transform_str))
-    transform_and_args = [Transformation(transform[0])] + transform[1:]
-    return transform_and_args, target_code
-
-
-def proc_line(line: str, transform_data: TransformData, target_code: list[str]):
-    TRANSFORMATION = "TRANSFORMATION: "
-    SCOP_IDX = "SCOP_IDX: "
-    NODE_IDX = "NODE_IDX: "
-    if line.startswith(COMMENT):
-        no_comment_line = line.replace(COMMENT, "")
-        if not no_comment_line:
-            return False
-        # if no_comment_line[0] != " ":
-        #     raise ValueError("In correctly formatted line")
-        stripped_line = no_comment_line[1:]
-        if stripped_line.startswith(TRANSFORMATION):
-            evaled = ast.literal_eval(stripped_line.replace(TRANSFORMATION, ""))
-            transform_data.transformation = Transformation(evaled[0])
-            transform_data.transformation_args = evaled[1:]
-            return True
-        elif stripped_line.startswith(SCOP_IDX):
-            return False
-        elif stripped_line.startswith(NODE_IDX):
-            return False
-
-    return False
-
-
-def foobar2(app):
-    results = []
-    target_code = []
-    transform_data = TransformData()
-    print(f"{transform_data=}")
-    with open(app.source) as file:
-        for line in file:
-            if proc_line(line, transform_data, target_code):
-                results.append(transform_data)
-                transform_data = TransformData()
-    print(f"{transform_data=}")
-    return results
-
-
 class TestCtadashi(unittest.TestCase):
-    def get_filtered_code(self, scops):
+    @staticmethod
+    def _proc_line(line: str, transform_data: TransformData, target_code: list[str]):
+        """Return `True` if the line contains transformation data.
+
+        Return `True` when the `transform_data` object gets populated, and
+        `False` when the line is appended to the `target_code` list.
+
+        """
+        TRANSFORMATION = " TRANSFORMATION: "
+        if line.startswith(COMMENT):
+            stripped_line = line.strip().replace(COMMENT, "")
+            if stripped_line.startswith(TRANSFORMATION):
+                transform_str = stripped_line.replace(TRANSFORMATION, "")
+                evaled = ast.literal_eval(transform_str)
+                transform_data.scop_idx = evaled[0]
+                transform_data.node_idx = evaled[1]
+                transform_data.transformation = Transformation(evaled[2])
+                transform_data.transformation_args = evaled[3:]
+                return True
+            else:
+                target_line = stripped_line[1:] if stripped_line else stripped_line
+                target_code.append(target_line)
+        return False
+
+    @classmethod
+    def _read_app_comments(cls, app):
+        transforms = []
+        target_code = []
+        transform_data = TransformData()
+        with open(app.source) as file:
+            for line in file:
+                if cls._proc_line(line, transform_data, target_code):
+                    transforms.append(transform_data)
+        return transforms, target_code
+
+    def _get_filtered_code(self, scops):
         with tempfile.TemporaryDirectory() as tmpdir:
             outfile = Path(tmpdir) / self._testMethodName
             outfile_bytes = str(outfile).encode()
@@ -86,25 +67,26 @@ class TestCtadashi(unittest.TestCase):
             generated_code = Path(outfile_bytes.decode()).read_text().split("\n")
         return [x for x in generated_code if not x.startswith(COMMENT)]
 
-    def test_lit(self):
-        app = Simple(source=Path(__file__).parent.parent / "threeloop.c")
-        transform_with_args, target_code = foobar(app)
-        foobar2(app)
+    def _lit(self, app):
+        transforms, target_code = self._read_app_comments(app)
 
         # transform
-        scop_idx = 0
-        node_idx = 2
         scops = Scops(app)
-        scop = scops[scop_idx]  # select_scop()
-        node = scop.schedule_tree[node_idx]  # model.select_node(scop)
-        node.transform(*transform_with_args)
+        for transform in transforms:
+            scop = scops[transform.scop_idx]  # select_scop()
+            node = scop.schedule_tree[transform.node_idx]  # model.select_node(scop)
+            node.transform(transform.transformation, *transform.transformation_args)
 
-        filtered_code = self.get_filtered_code(scops)
+        filtered_code = self._get_filtered_code(scops)
         diff = difflib.unified_diff(filtered_code, target_code)
         diff_str = "\n".join(diff)
         if diff_str:
             print(diff_str)
         self.assertFalse(diff_str)
+
+    def test_threeloop(self):
+        app = Simple(source=Path(__file__).parent.parent / "threeloop.c")
+        self._lit(app)
 
 
 if __name__ == "__main__":
