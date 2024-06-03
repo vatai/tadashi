787120a02a7a10f8f55ccdc0728d81e8f07a7327
Emil VATAI <emil.vatai@gmail.com>
2024-06-03 13:11:51 +0900

    Fill `partial_shift_var.c`

--- tests/py/partial_shift_var.c
+++ tests/py/partial_shift_var.c
@@ -1,8 +1,17 @@
-/// TRANSFORMATION: 0, 1, "PARTIAL_SHIFT_VAR", 0, 0
+/// TRANSFORMATION: 0, 5, "PARTIAL_SHIFT_VAR", 0, 0
+/// TRANSFORMATION: 0, 5, "PARTIAL_SHIFT_VAR", 0, 0
 /// #include <stdlib.h>
 ///
 /// void f(size_t N, double A[N][N]) {
 /// #pragma scop
+///   for(int c0 = 1; c0 < N; c0 += 1)
+///     for(int c1 = 0; c1 < N; c1 += 1)
+///       {
+///         for(int c2 = 2 * c0; c2 < N + 2 * c0; c2 += 1)
+///           A[c0][c1] = (A[c0][c1] + (A[c0 - 1][c1] * (-2 * c0 + c2)));
+///         for(int c2 = 0; c2 < N; c2 += 1)
+///           A[c0][c1] = ((A[c0][c1] + A[c0 - 1][c1]) + (c2));
+///       }
 /// #pragma endscop
 /// }
 ///
