e2755013436bcb99ff970ee82b79a9e8b15e7420
Emil VATAI <emil.vatai@gmail.com>
2024-07-18 22:23:17 +0900

    Indices look good

---
+++ tests/py/test_pluto_fig4.c
@@ -0,0 +1,23 @@
+/// TRANSFORMATION: 0, 8, "FULL_FUSE"
+/// TRANSFORMATION: 0, 8, "FULL_SHIFT_VAR",  2, 0
+/// TRANSFORMATION: 0, 8, "PARTIAL_SHIFT_VAL",  0, 1
+/// #include <stdlib.h>
+///
+/// void f(size_t N, size_t T, double a[N], double b[N]) {
+/// #pragma scop
+/// #pragma endscop
+/// }
+///
+#include <stdlib.h>
+
+void f(size_t N, size_t T, double a[N], double b[N]) {
+#pragma scop
+  const double third = 1. / 3.;
+  for (size_t t = 0; t < T; t++) {
+    for (size_t i = 2; i < N - 1; i++)
+      b[i] = third * (a[i - 1] + a[i] + a[i + 1]);
+    for (size_t j = 2; j < N - 1; j++)
+      a[j] = b[j];
+  }
+#pragma endscop
+}
