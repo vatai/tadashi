ea5e21882faa8b910d8df6463b3c0a67fae96689
Emil VATAI <emil.vatai@gmail.com>
2024-09-12 09:57:57 +0900

    Correct plots

--- times/plot_results.py
+++ times/plot_results.py
@@ -4,41 +4,85 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# https://stackoverflow.com/questions/50976297/reduce-a-panda-dataframe-by-groups
 
-def get_df(files):
+
+def get_df(files, agg_args, norm):
     means = {}
     for file in files:
         name = file.with_suffix("").name
         means[name] = json.load(open(file))
     df = pd.concat({k: pd.DataFrame(v) for k, v in means.items()})
     df.index.names = ["benchmark", "step"]
-    # https://stackoverflow.com/questions/50976297/reduce-a-panda-dataframe-by-groups
-    df = df.groupby(by=["benchmark"]).agg(
-        {
-            # "Random transformation": "min",
-            "Total walltime": "max",
-            "Kernel walltime": "min",
-            "Compilation": "min",
-            "Code generation": "mean",
-            "Transformation + legality": "mean",
-            "Extraction": "mean",
-        }
-    )
-    df["Total walltime"] = df["Total walltime"] - df["Kernel walltime"]
-    print(df)
-    print(df.sum(axis=1))
-    df = df.T / df.sum(axis=1)
-    print(df)
-    return df.T
+    # print(df)
+    gb = df.groupby(by=["benchmark"])
+    df = gb.agg(agg_args)
+    if "Total walltime" in df.columns and "Kernel walltime" in df.columns:
+        df["Total walltime (diff)"] = df["Total walltime"] - df["Kernel walltime"]
+        df = df.drop(["Total walltime"], axis=1)
+    if norm:
+        df = (df.T / df.sum(axis=1)).T
+    rename = {
+        "Total walltime": "Execution time",
+        "Kernel walltime": "Kernel execution",
+    }
+    df = df.rename(columns=rename)
+    print(df.columns)
+    return df
 
 
-def main(files):
-    means = get_df(list(files)[:3])
+def main(files, agg_args, norm, size):
+    df = get_df(files, agg_args, norm)
     fig, ax = plt.subplots()
-    means.plot.bar(stacked=True, ax=ax)
+    df.plot.bar(stacked=True, ax=ax)
+    if norm:
+        plt.ylabel("% of runtime")
+    else:
+        plt.ylabel("Runtime")
+    plt.title(
+        f"Breakdown of {size}-long transformation seq"
+        if size > 0
+        else "Breakdown of primitive transformations"
+    )
     plt.tight_layout()
-    plt.savefig("plot.pdf")
+    filename = f"plot-{'norm' if norm else 'abs'}-{size}.pdf"
+    print(filename)
+    plt.savefig(filename)
 
 
 if __name__ == "__main__":
-    main(files=Path("./times/").glob("*-10.json"))
+    agg_args = {
+        # "Random transformation": "min",
+        "Total walltime": "min",
+        # "Kernel walltime": "min",
+        "Compilation": "min",
+        "Code generation": "mean",
+        "Transformation + legality": "mean",
+        "Extraction": "mean",
+    }
+
+    main(
+        files=Path("./times/").glob("*-10.json"),
+        agg_args=agg_args,
+        norm=True,
+        size=10,
+    )
+
+    main(
+        files=Path("./times/").glob("*-10.json"),
+        agg_args=agg_args,
+        norm=False,
+        size=10,
+    )
+    main(
+        files=Path("./times/").glob("*-1.json"),
+        agg_args=agg_args,
+        norm=True,
+        size=1,
+    )
+    main(
+        files=Path("./times/").glob("*-1.json"),
+        agg_args=agg_args,
+        norm=False,
+        size=1,
+    )
