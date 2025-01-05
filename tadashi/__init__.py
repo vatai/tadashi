fc12c98077cf18976bacad3ba22c610768595ea8
Emil VATAI <emil.vatai@gmail.com>
2025-01-05 12:40:48 +0900

    Revert init

--- tadashi/__init__.py
+++ tadashi/__init__.py
@@ -13,7 +13,7 @@ from enum import Enum, StrEnum, auto
 from pathlib import Path
 from typing import Optional
 
-from ctadashi.pytadashi import pytadashi
+from build import pytadashi
 
 
 class AstLoopType(Enum):
