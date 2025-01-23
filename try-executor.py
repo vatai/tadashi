1bd24f718b0ce1fab31a7230c4dcf651220c22a6
Emil VATAI <emil.vatai@gmail.com>
2025-01-23 18:27:35 +0100

    Add try-except block

--- try-executor.py
+++ try-executor.py
@@ -16,8 +16,11 @@ def main():
     with futures.ThreadPoolExecutor(max_workers=4) as executor:
         fs = [executor.submit(func, *arg) for arg in enumerate(times[:])]
         print("-- scheduled --")
-        for f in futures.as_completed(fs, timeout=0):
-            print(f"{f.result()=}")
+        try:
+            for f in futures.as_completed(fs, timeout=0):
+                print(f"{f.result()=}")
+        except TimeoutError as e:
+            print(f"{e=}")
 
 
 if __name__ == "__main__":
