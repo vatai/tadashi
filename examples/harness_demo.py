99e39f9245cce3105237c4630e38b1cda8aed8e8
Emil VATAI <emil.vatai@gmail.com>
2025-01-30 12:11:06 +0900

    Copy executor code

--- examples/harness_demo.py
+++ examples/harness_demo.py
@@ -1,29 +1,29 @@
+from concurrent import futures
+
 import tadashi
 from tadashi.apps import Simple
 
-app = Simple("examples/depnodep.c")
-node = app.scops[0].schedule_tree[1]
-print(f"{node=}")
-tr = tadashi.TrEnum.FULL_SHIFT_VAR
-print(f"{tr in node.available_transformations=}")
-# output:
 
-print(f"{tr=}")
-lu = node.available_args(tr)
-print(f"{len(lu)=}")
-print(f"{lu[0]=}")
-print(f"{lu[1]=}")
-# output:
+def func():
+    pass
 
-args = [13, 1]
-print(f"{node.valid_args(tr, *args)=}")
-legal = node.transform(tr, *args)
-print(f"{legal=}")
-# output:
 
-app.compile()
-print(f"{app.measure()=}")
-app = app.generate_code()
-app.compile()
-print(f"{app.measure()=}")
-# output:
+def main():
+    app = Simple("examples/depnodep.c")
+    node = app.scops[0].schedule_tree[1]
+    tr = tadashi.TrEnum.FULL_SHIFT_VAR
+    lu = node.available_args(tr)
+    args = [13, 1]
+    legal = node.transform(tr, *args)
+
+    with futures.ThreadPoolExecutor(max_workers=4) as executor:
+        fs = [executor.submit(func, *arg) for arg in enumerate(times[:])]
+        print("-- scheduled --")
+        while fs:
+            done = [f for f in fs if f.done()]
+            for f in done:
+                print(f"{f.result()=}")
+                del fs[fs.index(f)]
+    app.compile()
+    app = app.generate_code()
+    app.compile()
