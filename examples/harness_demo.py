f69d0ce2b39ce1f7daa68f2d046246fd53a4720a
Emil VATAI <emil.vatai@gmail.com>
2025-02-11 09:43:31 +0900

    Dumb down harness_demo to make it work

--- examples/harness_demo.py
+++ examples/harness_demo.py
@@ -1,28 +1,28 @@
-from concurrent import futures
+#!/usr/bin/env python
+import socket
+from mpi4py import futures
+from mpi4py.futures import MPIPoolExecutor as Executor
 
 import tadashi
 from halo import Halo
 from tadashi.apps import App, Simple
 
-
-def func(app: App, scop_idx: int, transformation_list: list):
-    legal = app.scops[scop_idx].transform_list(transformation_list)
-    tapp = app.generate_code()
-    tapp.compile()
-    runtime = tapp.measure()
-    return legal, runtime
-
+def func(idx):
+    return f"FUNC({idx}) on {socket.gethostname()}"
 
 @Halo(spinner="dots")
 def main():
     app = Simple("examples/depnodep.c")
-    node = app.scops[0].schedule_tree[1]
     tlist = [
         [0, 1, tadashi.TrEnum.FULL_SHIFT_VAR, 13, 1],
     ]
-
-    with futures.ThreadPoolExecutor(max_workers=4) as executor:
-        fs = [executor.submit(app.transform_list, tlist)]
+    print(f"MAIN {socket.gethostname()=}")
+    fs = []
+    with Executor(max_workers=3) as executor:
+        for idx in range(3):
+            # fs.append(executor.submit(app.transform_list, tlist))
+            fs.append(executor.submit(func, idx))
+        print(f"{len(fs)=}")
         while fs:
             done = [f for f in fs if f.done()]
             for f in done:
