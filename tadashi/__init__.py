071420cc1ac1d1dd97e80b2a51a02f974f243794
Emil VATAI <emil.vatai@gmail.com>
2025-02-14 17:54:59 +0900

    Fix num args

--- tadashi/__init__.py
+++ tadashi/__init__.py
@@ -401,6 +401,7 @@ class PartialShiftParamInfo(TransformInfo):
 
 class SetParallelInfo(TransformInfo):
     func_name = "set_parallel"
+    arg_help = ["omp num_threads"]
 
 
 class SetLoopOptInfo(TransformInfo):
