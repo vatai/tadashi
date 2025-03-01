6ab793fc2354a8afbebf75472d7ebda5b1078c4d
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-03-01 22:31:35 +0900

    manual transformations

--- tadashi/mcts/__init__.py
+++ tadashi/mcts/__init__.py
@@ -99,10 +99,3 @@ class MCTSNode_Node(MCTSNode):
             return
         # TODO: clone the app
         # TODO: repeat to the node selection again
-
-if __name__ == "__main__":
-    app = Polybench("linear-algebra/blas/gemm", "./examples/polybench/", compiler_options=["-D", "LARGE_DATASET"])
-    root = MCTSNode(app=app, action="START")
-    root.select_node()
-    print("samples tree as follows:")
-    root.print()
