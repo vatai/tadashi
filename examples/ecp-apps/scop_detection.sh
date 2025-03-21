f72be52f94063c4da4d161e6c2c157203cf4e37c
Emil VATAI <emil.vatai@gmail.com>
2025-03-21 17:26:04 +0900

    Include .C files

--- examples/ecp-apps/scop_detection.sh
+++ examples/ecp-apps/scop_detection.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-ls $1/**/*.c* | while read -r file; do ../../build/scop_detector "$file"; done 2>/dev/null
+ls $1/**/*.c* $1/**/*.C* | while read -r file; do ../../build/scop_detector "$file"; done 2>/dev/null
