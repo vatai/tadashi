f436578ca4a1e10e024aba9cf9e62d28f2ac862b
Emil VATAI <emil.vatai@gmail.com>
2025-03-25 14:20:39 +0900

    Some update

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-clang -S -emit-llvm ./examples/inputs/depnodep.c -O1 -o - | opt -load LLVMPolly.so -polly-canonicalize -polly-import-jscop -o depnodep.mod.ll && clang depnodep.mod.ll  -o depnodep.mod
+clang -S -emit-llvm ./examples/inputs/depnodep.c -O1 -o - | opt -load LLVMPolly.so -disable-polly-legality -polly-canonicalize -polly-import-jscop -o depnodep.mod.ll && clang depnodep.mod.ll  -o depnodep.mod
 ./depnodep.orig > out.orig
 ./depnodep.mod  > out.mod
 diff out.orig out.mod
