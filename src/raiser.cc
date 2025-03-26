ea22882eaa5d5f09896fc1a109a892cc0633d0c0
Emil VATAI <emil.vatai@gmail.com>
2025-03-26 15:28:42 +0900

    Union domain and schedule

--- src/raiser.cc
+++ src/raiser.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <isl/union_map_type.h>
+#include <isl/union_set.h>
 #include <nlohmann/json.hpp>
 
 #include <isl/ctx.h>
@@ -20,19 +21,27 @@ main(int argc, char *argv[]) {
                         "preheader19---%56.jscop");
   json data;
   istream >> data;
+  // data.keys: arrays,context,name,statements,
   auto stmts = data["statements"];
-  isl_union_map *union_theta = isl_union_map_empty_ctx(ctx);
-  for (auto stmt : stmts.items()) {
-    auto name = stmt.value()["name"];
-    auto schedule = stmt.value()["schedule"];
-    std::stringstream ss;
-    ss << schedule.template get<std::string>();
-    isl_union_map *theta = isl_union_map_read_from_str(
-        ctx, schedule.template get<std::string>().c_str());
-    std::cout << "Isl schedule: " << isl_union_map_to_str(theta) << std::endl;
-    union_theta = isl_union_map_union(union_theta, theta);
+  isl_union_map *union_schedule = isl_union_map_empty_ctx(ctx);
+  isl_union_set *union_domain = isl_union_set_empty_ctx(ctx);
+  for (auto &stmt : stmts) {
+    auto accesses = stmt["accesses"];
+    auto name = stmt["name"];
+
+    isl_union_set *domain = isl_union_set_read_from_str(
+        ctx, stmt["domain"].template get<std::string>().c_str());
+    union_domain = isl_union_set_union(union_domain, domain);
+
+    isl_union_map *schedule = isl_union_map_read_from_str(
+        ctx, stmt["schedule"].template get<std::string>().c_str());
+    std::cout << "Isl schedule: " << isl_union_map_to_str(schedule)
+              << std::endl;
+    union_schedule = isl_union_map_union(union_schedule, schedule);
   }
-  std::cout << "Union Theta: " << isl_union_map_to_str(union_theta)
+  isl_schedule *schedule = isl_schedule_from_domain(union_domain);
+  std::cout << isl_schedule_to_str(schedule) << std::endl;
+  std::cout << "Union Theta: " << isl_union_map_to_str(union_schedule)
             << std::endl;
   std::printf("Done!\n");
   return 0;
