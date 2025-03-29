820e16ba883df06aca4e70b46ce7feefbe9de871
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-03-29 22:38:27 +0900

    rename

--- manual_gemm.py
+++ manual_gemm.py
@@ -2,6 +2,8 @@ from tadashi import TrEnum
 from tadashi.apps import Simple
 
 app = Simple("./examples/inputs/simple/gemm.c")
+app.compile()
+app.measure()
 print(app.scops[0].schedule_tree[0].yaml_str)
 
 app.scops[0].transform_list([[1, TrEnum.TILE, 16]])
@@ -13,4 +15,6 @@ tile2.scops[0].transform_list([[3, TrEnum.TILE, 16]])
 
 tile2.scops[0].transform_list([[2, TrEnum.INTERCHANGE]])
 final_app = tile2.generate_code("final.c")
+final_app.compile()
 final_app.measure()
+print("all done")
