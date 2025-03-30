dfe6a0adfcd595fc3138fd273372f5ecce363894
Emil VATAI <emil.vatai@gmail.com>
2025-03-30 22:32:14 +0900

    Add `make_new_app` and compiler property

--- tadashi/apps.py
+++ tadashi/apps.py
@@ -46,6 +46,10 @@ class App:
         app.ephemeral = True
         return app
 
+    def make_new_app(self, ephemeral, **kwargs):
+        kwargs["compiler_options"] = self.user_compiler_options
+        return self.make_ephemeral(**kwargs) if ephemeral else self.__class__(**kwargs)
+
     def __del__(self):
         if self.ephemeral:
             self.remove_binary()
@@ -58,6 +62,11 @@ class App:
 
     def remove_source(self):
         self.source.unlink()
+
+    @staticmethod
+    @property
+    def compiler():
+        return os.getenv("CC", "gcc")
 
     @property
     def compile_cmd(self) -> list[str]:
@@ -91,6 +100,8 @@ class App:
     def compile(self):
         """Compile the app so it can be measured/executed."""
         cmd = self.compile_cmd + self.user_compiler_options
+        # print(f"{' '.join(cmd)}")
+        print(cmd)
         result = subprocess.run(cmd)
         # raise an exception if it didn't compile
         result.check_returncode()
@@ -145,7 +156,7 @@ class Simple(App):
     @property
     def compile_cmd(self) -> list[str]:
         return [
-            "gcc",
+            self.compiler,
             str(self.source),
             "-fopenmp",
             "-o",
@@ -168,11 +179,8 @@ class Simple(App):
             prefix = f"{filename}-{now_str}"
             new_file = Path(tempfile.mktemp(prefix=prefix, suffix=suffix, dir="."))
         self.scops.generate_code(self.source, Path(new_file))
-        kwargs = {
-            "source": new_file,
-            "compiler_options": self.user_compiler_options,
-        }
-        return Simple.make_ephemeral(**kwargs) if ephemeral else Simple(**kwargs)
+        kwargs = {"source": new_file}
+        return self.make_new_app(ephemeral, **kwargs)
 
 
 class Polybench(App):
@@ -215,7 +223,7 @@ class Polybench(App):
     @property
     def compile_cmd(self) -> list[str]:
         return [
-            "gcc",
+            self.compiler,
             str(self.source),
             str(self.utilities / "polybench.c"),
             "-DPOLYBENCH_TIME",
@@ -237,9 +245,8 @@ class Polybench(App):
             "benchmark": self.benchmark,
             "base": self.base,
             "infix": alt_infix,
-            "compiler_options": self.user_compiler_options,
         }
-        return Polybench.make_ephemeral(**kwargs) if ephemeral else Polybench(**kwargs)
+        return self.make_new_app(ephemeral, **kwargs)
 
     @staticmethod
     def _source_with_infix(source: Path, infix: str):
