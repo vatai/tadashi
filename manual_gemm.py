f33b1251d9c7af15fa6fe56af140b3df4993d32c
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-04-03 10:59:19 +0900

    manual gemm

---
+++ manual_gemm.py
@@ -0,0 +1,34 @@
+import tadashi
+from tadashi import TrEnum
+from tadashi.apps import Polybench, Simple
+
+base = "examples/polybench"
+
+# gemm = Polybench(
+#     "linear-algebra/blas/gemm",
+#     base,
+#     compiler_options=["-DEXTRALARGE_DATASET", "-O3"],
+# )
+gemm = Simple("./examples/inputs/simple/gemm.c")
+print(f"{gemm.user_compiler_options=}")
+
+gemm.compile()
+print(f"{gemm.measure()=}")
+for tile_size in [64, 100, 128]:
+    gemm.reset_scops()
+    trs = [
+        [0, 7, TrEnum.TILE, tile_size],
+        [0, 9, TrEnum.TILE, tile_size],
+        [0, 11, TrEnum.TILE, tile_size],
+        [0, 8, TrEnum.INTERCHANGE],
+        [0, 10, TrEnum.INTERCHANGE],
+        [0, 9, TrEnum.INTERCHANGE],
+    ]
+    gemm.transform_list(trs)
+    # print(gemm.scops[0].schedule_tree[9].yaml_str)
+    tiled = gemm.generate_code(alt_infix="_tiled{tile_size}", ephemeral=False)
+
+    tiled.compile()
+    print(f"{tile_size=} : {tiled.measure()=}")
+
+print("DONE")
