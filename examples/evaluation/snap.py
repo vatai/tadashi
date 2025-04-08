7d88ac9dfc421aa9f94bf78afca9fd84c7d2c28c
Emil VATAI <emil.vatai@gmail.com>
2025-04-08 17:48:27 +0900

    Deatailed print

--- examples/evaluation/snap.py
+++ examples/evaluation/snap.py
@@ -67,6 +67,8 @@ class Snap(App):
 snap = Snap(Path(__file__).parent / "SNAP/ports/snap-c/dim1_sweep.c", 1)
 tr = TrEnum.INTERCHANGE
 for node_idx, node in enumerate(snap.scops[0].schedule_tree):
+    print(f"{node_idx=}")
+    print(f"{node.available_transformations=}")
     if tr in node.available_transformations:
         print(f"{node_idx=}")
         node.transform(tr)
