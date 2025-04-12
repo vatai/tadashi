fb685c66111d0b73fdd391fddb3858c036ec1121
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-04-12 22:11:40 +0900

    use optimize code from mcts

--- examples/evaluation/miniAMR/🌳.py
+++ examples/evaluation/miniAMR/🌳.py
@@ -1,3 +1,9 @@
-from tadashi.mcts.node_root import MCTSNode_Root
+from tadashi.mcts.optimize import optimize_app
 
 print("oi")
+from app import miniAMR
+
+app = miniAMR()
+
+print(f"{len(app.scops)}")
+
