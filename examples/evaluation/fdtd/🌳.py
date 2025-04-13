a8f53cd0599330c793027fe96868b22a3c252f48
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-04-13 20:28:48 +0900

    add mcts entry point

---
+++ examples/evaluation/fdtd/🌳.py
@@ -0,0 +1,24 @@
+from tadashi import TrEnum
+from tadashi.mcts.optimize import optimize_app
+
+from fdtd.app import MakefileApp
+
+app = MakefileApp("fdtd/fd3d_4.2.c")
+
+print(f"cnt scops: {len(app.scops)}")
+
+allowed_transformations = {
+    TrEnum.TILE1D,
+    TrEnum.TILE2D,
+    TrEnum.TILE3D,
+    TrEnum.INTERCHANGE,
+    TrEnum.FULL_FUSE,
+    #TrEnum.SPLIT,
+    TrEnum.FULL_SPLIT,
+}
+
+optimize_app(app,
+             rollouts=10000,
+             scop_idx=0,
+             whitelist_transformations=allowed_transformations)
+print("all done")
