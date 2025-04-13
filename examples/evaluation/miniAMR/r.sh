a21c88af5870c3a2c0af00b804aa267c2b3f6d6c
Emil VATAI <emil.vatai@gmail.com>
2025-04-13 10:08:18 +0900

    Remove email

--- examples/evaluation/miniAMR/r.sh
+++ examples/evaluation/miniAMR/r.sh
@@ -4,7 +4,6 @@
 #SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
-#SBATCH --mail-user=alexander.drozd@gmail.com
 
 # ======== Modules ========
 #source /etc/profile.d/modules.sh
