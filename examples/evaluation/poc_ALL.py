0cd0282347605c85b2c02ff5ff0a9e6dd9a82a4f
João Batista <jesp.batista@gmail.com>
2025-04-14 15:57:05 +0900

    Update poc_ALL.py

--- examples/evaluation/poc_ALL.py
+++ examples/evaluation/poc_ALL.py
@@ -1,10 +1,9 @@
-
 import tadashi
 from tadashi import TrEnum
 from tadashi.apps import Polybench
 
 
-base = "PolyBenchC-4.2.1/"
+base = "examples/polybench/"
 
 
 app_names = [
@@ -41,6 +40,11 @@ app_names = [
 ]
 
 
+apps_miniAMR = [
+"examples/evaluation/miniAMR/",
+]
+
+
 def searchFor(app, tr_name):
 	scops = app.scops 
 	ret = []
@@ -62,7 +66,7 @@ for app_name in app_names:
 	app = Polybench(
 		app_name,
 		base,
-		compiler_options=["-DEXTRALARGE_DATASET", "-DLARGE_DATASET", "-O3"][1::],
+		compiler_options=["-DEXTRALARGE_DATASET", "-DMINI_DATASET", "-O3"][::2],
 	)
 
 
@@ -77,11 +81,23 @@ for app_name in app_names:
 
 	tile_size = 32
 
+	scops = app.scops
 
 	trs = searchFor(app, "full_split")
-	trs = [ [0, index, TrEnum.FULL_SPLIT] for index in trs ]
-	app.transform_list(trs[::-1])
-	full_tr_list.extend(trs[::-1])
+	trs = [ [index, TrEnum.FULL_SPLIT] for index in trs ]
+	trs = trs[::-1]
+	for t in trs:
+		scops[0].reset()
+		scops[0].transform_list(full_tr_list)
+		valid = scops[0].transform_list([t])
+		if valid[-1]:
+			full_tr_list.append(t)
+		else:
+			print("skipped tr:", str(t))
+	scops[0].reset()
+	valid = scops[0].transform_list(full_tr_list)
+	print("FULL_SPLIT list validity:", valid)
+	#full_tr_list.extend(trs[::-1])
 
 	trs = searchFor(app, "tile3d")
 	toRemoveFrom2D = [ a for a in trs]
@@ -90,32 +106,45 @@ for app_name in app_names:
 	for t in trs:
 		if t-1 in trs:
 			trs.pop(trs.index(t-1))		
-	trs3D = [ [0, index, TrEnum.TILE3D, tile_size, tile_size, tile_size] for index in trs[::-1] ]
+	trs3D = [ [index, TrEnum.TILE3D, tile_size, tile_size, tile_size] for index in trs[::-1] ]
 	trs2 = searchFor(app, "tile2d")
-	for t in toRemoveFrom2D:
-		if t in trs2:
-			trs2.pop(trs2.index(t))
-	trs2D = [ [0, index, TrEnum.TILE2D, tile_size, tile_size] for index in trs2[::-1] ]
+	#for t in toRemoveFrom2D:
+	#	if t in trs2:
+	#		trs2.pop(trs2.index(t))
+	trs2D = [ [index, TrEnum.TILE2D, tile_size, tile_size] for index in trs2[::-1] ]
 	trs3D.extend(trs2D)
 	trs3D.sort()
-	app.transform_list(trs3D[::-1])
-	full_tr_list.extend(trs3D[::-1])
+	trs3D = trs3D[::-1]
+	for t in trs3D:
+		scops[0].reset()
+		scops[0].transform_list(full_tr_list)
+		valid = scops[0].transform_list([t])
+		if valid[-1]:
+			full_tr_list.append(t)
+		else:
+			print("skipped tr:", str(t))
+	scops[0].reset()
+	valid = scops[0].transform_list(full_tr_list)
+	print("TILE 2D and 3D list validity:", valid)
+	#full_tr_list.extend(trs3D[::-1])
 
 	#trs = searchFor(app, "full_fuse")
 	#trs = [ [0, index, TrEnum.FULL_FUSE] for index in trs ]
 	#app.transform_list(trs[::-1])
 	#full_tr_list.extend(trs[::-1])
+	#full_tr_list = [ [0]+l for l in full_tr_list]
 
 	print("transformation_list=[")
 	[print('   %s,'%str(t) ) for t in full_tr_list]
 	print("]")
 
 	for tile_size in [32]: 
-		app.reset_scops()
+		scops[0].reset()
 
 		print("Tiling with size %d ..." % tile_size)
 
-		app.transform_list(full_tr_list)
+		valid = scops[0].transform_list(full_tr_list)
+		print("Is this transformation list valid:", valid)
 
 
 		tiled = app.generate_code(alt_infix="_tiled%d"%tile_size, ephemeral=False)
@@ -125,5 +154,3 @@ for app_name in app_names:
 
 
 	print("[FINISHED APP]\n\n")
-
-
