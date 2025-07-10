eb81517785769a4f334e0f7f698c2a0138b8ac9e
Emil VATAI <emil.vatai@gmail.com>
2025-07-10 16:12:24 +0900

    Try removing `--with-clang-prefix`

--- deps/install_scripts/pet.sh
+++ deps/install_scripts/pet.sh
@@ -10,7 +10,8 @@ CONFIGURE_ARGS=(
   --enable-shared
   --prefix="$1" # ${CTADASHI_INSTALL_PREFIX}
   --libdir="$2" # ${CTADASHI_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}
-  --with-clang-prefix="$3" # ${CMAKE_SOURCE_DIR}/deps/opt
+  # --with-clang-prefix="$3" # ${CMAKE_SOURCE_DIR}/deps/opt
+  # --with-clang-prefix=/usr/lib/llvm19 # ${CMAKE_SOURCE_DIR}/deps/opt
 )
 ./get_submodules.sh
 git -C ./isl checkout master
