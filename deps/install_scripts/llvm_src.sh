89e2f3b0806b04b4ae941d6bf259ab4fd971785f
Emil VATAI <emil.vatai@gmail.com>
2025-07-10 16:11:45 +0900

    Update version of LLVM in the install script

--- deps/install_scripts/llvm_src.sh
+++ deps/install_scripts/llvm_src.sh
@@ -3,11 +3,13 @@
 TADASHI_DEPS_PREFIX=${TADASHI_DEPS_PREFIX:-$(git rev-parse --show-toplevel)/deps/opt}
 mkdir -p "$TADASHI_DEPS_PREFIX"
 
+version=20.1.8
+
 pushd /tmp || exit
-# git clone --depth 1 --branch llvmorg-19.1.7 https://github.com/llvm/llvm-project.git
-wget -c https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.7/llvm-project-19.1.7.src.tar.xz
-tar xvf llvm-project-19.1.7.src.tar.xz
-pushd llvm-project-19.1.7.src || exit
+# git clone --depth 1 --branch llvmorg-${version} https://github.com/llvm/llvm-project.git
+wget -c https://github.com/llvm/llvm-project/releases/download/llvmorg-${version}/llvm-project-${version}.src.tar.xz
+tar xvf llvm-project-${version}.src.tar.xz
+pushd llvm-project-${version}.src || exit
 cmake -S llvm -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${TADASHI_DEPS_PREFIX}" -DLLVM_ENABLE_PROJECTS="clang;flang;polly;mlir"
 ninja -C build install
 popd || exit
