9687e9666a5d3b07aafcc62aae88606f23e3f2a0
Emil VATAI <emil.vatai@gmail.com>
2025-08-22 15:58:36 +0900

    Finish generate pluto files script

--- scripts/pluto/generate_pluto_c_files.sh
+++ scripts/pluto/generate_pluto_c_files.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# set -x
+
+PLUTO=./deps/build/pluto-0.13.0/polycc
+
+find "$(realpath "$1")" -name '*.c' | grep -v polybench/utilities | while read -r file; do
+    cd "$(dirname "$file")" || exit
+    $PLUTO $file
+    gcc -o "${file%.c}.pluto.x" "${file%.c}.pluto.c" "$1/utilities/polybench.c" -I "$1/utilities/polybench.c"
+    cd - > /dev/null || exit
+done
+
 echo "done"
-find $1 -name '*.c'
