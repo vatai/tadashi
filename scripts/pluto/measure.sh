7a9e8e5e483a54f150d1e7c649dd45e2c78a8c37
Emil VATAI <emil.vatai@gmail.com>
2025-08-23 06:51:44 +0900

    Small fixes

--- scripts/pluto/measure.sh
+++ scripts/pluto/measure.sh
@@ -11,10 +11,12 @@ readarray -d '' BENCHMARKS < <(find "$POLYBENCH_ROOT" -name '*.pluto.x' |
                                    tr "\n" "\0")
 rm "$OUTFILE"
 for file in "${BENCHMARKS[@]}"; do
+    echo "$file"
     echo -n -e "$(basename "${file%.pluto.x}")\t" >> "$OUTFILE"
     for _ in $(seq "$NUM_REPS"); do
         $file | tr "\n" "\t" >> "$OUTFILE"
     done
+    echo "" >> "$OUTFILE"
 done
 
 echo "done"
