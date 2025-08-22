4fe0509f341e624a8f08cbc0dbbfc2dcfa7e33dc
Emil VATAI <emil.vatai@gmail.com>
2025-08-22 16:02:25 +0900

    Fixes: round 1

--- scripts/pluto/generate_pluto_c_files.sh
+++ scripts/pluto/generate_pluto_c_files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# set -x
+set -x
 
-PLUTO=./deps/build/pluto-0.13.0/polycc
+REPO_ROOT=$(git rev-parse --show-toplevel)
+PLUTO=${PLUTO:-$REPO_ROOT/deps/build/pluto-0.13.0/polycc}
+POLYBENCH_ROOT="$(readpath $1)"
 
-find "$(realpath "$1")" -name '*.c' | grep -v polybench/utilities | while read -r file; do
+find "$POLYBENCH_ROOT" -name '*.c' | grep -v polybench/utilities | while read -r file; do
     cd "$(dirname "$file")" || exit
     $PLUTO $file
     gcc -o "${file%.c}.pluto.x" "${file%.c}.pluto.c" "$1/utilities/polybench.c" -I "$1/utilities/polybench.c"
