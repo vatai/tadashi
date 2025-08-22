772fd89665c3f58cf0e4e045a8cf9188523c7aa4
Emil VATAI <emil.vatai@gmail.com>
2025-08-23 06:48:53 +0900

    Add measure.sh

---
+++ scripts/pluto/measure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set -x
+
+REPO_ROOT="$(realpath "$(git rev-parse --show-toplevel)")"
+POLYBENCH_ROOT="$REPO_ROOT/examples/polybench"
+NUM_REPS=10
+OUTFILE="pluto_times_${NUM_REPS}.csv"
+
+readarray -d '' BENCHMARKS < <(find "$POLYBENCH_ROOT" -name '*.pluto.x' |
+                                   tr "\n" "\0")
+rm "$OUTFILE"
+for file in "${BENCHMARKS[@]}"; do
+    echo -n -e "$(basename "${file%.pluto.x}")\t" >> "$OUTFILE"
+    for _ in $(seq "$NUM_REPS"); do
+        $file | tr "\n" "\t" >> "$OUTFILE"
+    done
+done
+
+echo "done"
