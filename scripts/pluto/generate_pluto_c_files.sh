0c39a68edcf9e7cafd70a3b38e90c988ad1285c7
Emil VATAI <emil.vatai@gmail.com>
2025-08-22 16:03:06 +0900

    Fix typo

--- scripts/pluto/generate_pluto_c_files.sh
+++ scripts/pluto/generate_pluto_c_files.sh
@@ -4,7 +4,7 @@ set -x
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 PLUTO=${PLUTO:-$REPO_ROOT/deps/build/pluto-0.13.0/polycc}
-POLYBENCH_ROOT="$(readpath $1)"
+POLYBENCH_ROOT="$(realpath $1)"
 
 find "$POLYBENCH_ROOT" -name '*.c' | grep -v polybench/utilities | while read -r file; do
     cd "$(dirname "$file")" || exit
