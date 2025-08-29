500e3c512ad18959a104673adcf9834e1f38ae94
Emil VATAI <emil.vatai@gmail.com>
2025-08-29 16:30:03 +0900

    Add evol_tadashi.sh script

---
+++ scripts/genoa/run/evol_tadashi.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+#SBATCH -p genoa
+#SBATCH -N 1
+#SBATCH -t 10:00:00
+# #SBATCH -n 1
+# #SBATCH -c 1
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+source "${REPO_ROOT}/deps/set_env.src"
+set_env "${REPO_ROOT}/deps/opt"
+
+PYTHONPATH="${REPO_ROOT}" python "${REPO_ROOT}/examples/evaluation/evol_tadashi.py"
