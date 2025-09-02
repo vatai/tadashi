a5b3a39129054bc23c340e776e34a678e47e5917
Emil VATAI <emil.vatai@gmail.com>
2025-09-02 17:46:04 +0900

    Add pluto submission script

---
+++ examples/evaluation/pluto/genoa.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# USAGE: sbatch genoa.sh
+
+#SBATCH -p genoa
+#SBATCH -N 1
+#SBATCH -t 5:00:00
+#SBATCH -o pluto-%x-%j.txt
+#SBATCH -e pluto-%x-%j.txt
+
+BASE="$(dirname "$0")"
+"$BASE/compile.sh"
+"$BASE/measure.sh"
+
