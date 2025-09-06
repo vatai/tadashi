24f743fef85184820b70e8b9ac1a71e20043ab41
Emil VATAI <emil.vatai@gmail.com>
2025-09-06 21:59:35 +0900

    Better string comparison

--- examples/evaluation/poc/measure_st.sh
+++ examples/evaluation/poc/measure_st.sh
@@ -25,7 +25,7 @@ readarray -d '' BENCHMARKS < <(find "$POLYBENCH_ROOT" -name '*.c' |
                                    grep -v pluto.c |
                                    tr "\n" "\0")
 hostname
-test $(hostname) == genoa12.cloud.r-ccs.riken.jp && exit
+test "x$(hostname)" == xgenoa12.cloud.r-ccs.riken.jp && exit
 echo "OMP_NUM_THREADS=$OMP_NUM_THREADS"
 for file in "${BENCHMARKS[@]}"; do
     # echo "$file"
