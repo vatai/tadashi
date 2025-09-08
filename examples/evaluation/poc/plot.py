e110252d1062a809d3537d3d138ab9562f14d646
Emil VATAI <emil.vatai@gmail.com>
2025-09-08 18:33:23 +0900

    Fix missing colon

--- examples/evaluation/poc/plot.py
+++ examples/evaluation/poc/plot.py
@@ -38,7 +38,7 @@ def read_poc_output(path):
             if ".orig" in f[line_i + 1]:
                 continue
             app_name = f[line_i + 1]
-            if ": " in f[line_i + 3]
+            if ": " in f[line_i + 3]:
                 app_baseline = f[line_i + 3]
             else:
                 app_baseline = f[line_i + 4]
