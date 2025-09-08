12906ede1c15941014ed2b5ce18478e8b7b73673
Emil VATAI <emil.vatai@gmail.com>
2025-09-08 21:47:17 +0900

    Restart mcts all.sh

--- examples/evaluation/mcts/measure_mt.sh
+++ examples/evaluation/mcts/measure_mt.sh
@@ -7,30 +7,13 @@
 #SBATCH -t 20:00:00
 #SBATCH -o %x-%j.txt
 #SBATCH -e %x-%j.txt
-#SBATCH -n 192
-# set -x
 
+### SET UP ENV ###
 REPO_ROOT="$(realpath "$(git rev-parse --show-toplevel)")"
 source "${REPO_ROOT}/scripts/genoa.source"
-POLYBENCH_ROOT="$REPO_ROOT/examples/polybench"
-NUM_REPS=10
-SIZE=EXTRALARGE
-OFLAG=3
-# export OMP_NUM_THREADS=1
 
-readarray -d '' BENCHMARKS < <(find "$POLYBENCH_ROOT" -name '*.c' |
-	grep -v polybench/utilities |
-	grep -v TMPFILE |
-	grep -v INFIX |
-	grep -v origi.c |
-	grep -v pluto.c |
-	tr "\n" "\0")
 hostname
 test "x$(hostname)" == xgenoa12.cloud.r-ccs.riken.jp && exit
 echo "OMP_NUM_THREADS=$OMP_NUM_THREADS"
-for file in "${BENCHMARKS[@]}"; do
-	benchmark="$(basename "${file%.c}")"
-	python3 ./mcts_polybench.py "${benchmark}" --rollouts=3000 --prefix=data-mt --allow-omp "$@"
-done
+python3 ./mcts_polybench.py "${SLURM_JOB_NAME}" --rollouts=5000 --prefix=data-mt --allow-omp "$@"
 
-echo "done"
