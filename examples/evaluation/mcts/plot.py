1b563c69032d61cb0e719d96bc024e7ddbbdb71f
Emil VATAI <emil.vatai@gmail.com>
2025-09-10 20:37:06 +0900

    mcts: Nice outputs for plot.py

--- examples/evaluation/mcts/plot.py
+++ examples/evaluation/mcts/plot.py
@@ -1,4 +1,5 @@
 #!/bin/env python
+import argparse
 import json
 from pathlib import Path
 
@@ -102,92 +103,99 @@ class AnchoredScaleBar(mpl.offsetbox.AnchoredOffsetbox):
         )
 
 
-fig = plt.figure(figsize=(6, 12))
-i = 0
-ax_objs = []
-directory = Path("./data")
-json_files = list(sorted(directory.glob("*.jsonl")))
+def main(datadir):
+    fig = plt.figure(figsize=(6, 12))
+    i = 0
+    ax_objs = []
+    directory = Path(datadir)
+    json_files = list(sorted(directory.glob("*.jsonl")))
 
-# cmap = plt.colormaps['brg']
-# cmap = plt.colormaps['ocean']
+    # cmap = plt.colormaps['brg']
+    # cmap = plt.colormaps['ocean']
 
-cmap = sns.color_palette("blend:#21599c,#b59610", as_cmap=True)
-# cmap = sns.color_palette("blend:#7AB,#EDA", as_cmap=True)
-colors = [cmap(i) for i in np.linspace(0, 1, len(json_files) + 3)]  # Evenly spaced
-# colors = sns.color_palette('tab20', n_colors=len(json_files))
-# colors = sns.cubehelix_palette(len(json_files), rot=-.25, light=.7)
+    cmap = sns.color_palette("blend:#21599c,#b59610", as_cmap=True)
+    # cmap = sns.color_palette("blend:#7AB,#EDA", as_cmap=True)
+    colors = [cmap(i) for i in np.linspace(0, 1, len(json_files) + 3)]  # Evenly spaced
+    # colors = sns.color_palette('tab20', n_colors=len(json_files))
+    # colors = sns.cubehelix_palette(len(json_files), rot=-.25, light=.7)
 
-gs = grid_spec.GridSpec(len(json_files), 1)
-final_speedups = {}
-for f in json_files:
-    cnt_evals, speedups = parse_jsonl(f)
-    ax_objs.append(fig.add_subplot(gs[i : i + 1, 0:]))
-    ax = ax_objs[-1]
-    # ax.plot(cnt_evals, speedups,
-    #         drawstyle="steps-post",
-    #         color="#000000")
-    ax.fill_between(cnt_evals, speedups, alpha=0.9, step="post", color=colors[i])
-    ax.set_xlim(0, 3000)
-    ax.set_ylim(0, 100)
+    gs = grid_spec.GridSpec(len(json_files), 1)
+    final_speedups = {}
+    for f in json_files:
+        cnt_evals, speedups = parse_jsonl(f)
+        ax_objs.append(fig.add_subplot(gs[i : i + 1, 0:]))
+        ax = ax_objs[-1]
+        # ax.plot(cnt_evals, speedups,
+        #         drawstyle="steps-post",
+        #         color="#000000")
+        ax.fill_between(cnt_evals, speedups, alpha=0.9, step="post", color=colors[i])
+        ax.set_xlim(0, 3000)
+        ax.set_ylim(0, 100)
 
-    # make background transparent
-    rect = ax_objs[-1].patch
-    rect.set_alpha(0)
+        # make background transparent
+        rect = ax_objs[-1].patch
+        rect.set_alpha(0)
 
-    # remove borders, axis ticks, and labels
-    ax_objs[-1].set_yticklabels([])
+        # remove borders, axis ticks, and labels
+        ax_objs[-1].set_yticklabels([])
 
-    if i == len(json_files) - 1:
-        ax_objs[-1].set_xlabel(
-            "Number of Evaluations",
-            # fontsize=16,
-            # fontweight="bold"
+        if i == len(json_files) - 1:
+            ax_objs[-1].set_xlabel(
+                "Number of Evaluations",
+                # fontsize=16,
+                # fontweight="bold"
+            )
+        else:
+            ax_objs[-1].set_xticklabels([])
+
+        # spines = ["top","right","left","bottom"]
+        # ax.spines[['right', 'top', 'left', 'bottom']].
+        spines = ["top", "right", "left"]
+        for s in spines:
+            ax.spines[s].set_visible(False)
+        # ax.spines["bottom"].set_linestyle(":")
+        ax.spines["bottom"].set_linestyle((0, (4, 4)))
+        ax.spines["bottom"].set_linewidth(0.5)
+        kernel = f.name.split(".")[0]
+        final_speedups[kernel] = speedups[-1]
+        # print(f)
+        ax_objs[-1].text(
+            -20,
+            0,
+            kernel + f"\n({speedups[-1]:0.1F}x)",
+            fontsize=12,
+            ha="right",
+            # color=colors[i]
         )
-    else:
-        ax_objs[-1].set_xticklabels([])
+        i += 1
 
-    # spines = ["top","right","left","bottom"]
-    # ax.spines[['right', 'top', 'left', 'bottom']].
-    spines = ["top", "right", "left"]
-    for s in spines:
-        ax.spines[s].set_visible(False)
-    # ax.spines["bottom"].set_linestyle(":")
-    ax.spines["bottom"].set_linestyle((0, (4, 4)))
-    ax.spines["bottom"].set_linewidth(0.5)
-    kernel = f.name.split(".")[0]
-    final_speedups[kernel] = speedups[-1]
-    # print(f)
-    ax_objs[-1].text(
-        -20,
-        0,
-        kernel + f"\n({speedups[-1]:0.1F}x)",
-        fontsize=12,
-        ha="right",
-        # color=colors[i]
+    df = pd.Series(final_speedups)
+    df.rename("mcts", inplace=True)
+    df.index.name = "benchmark"
+    gs.update(hspace=-0.57)
+    ob = AnchoredScaleBar(
+        size=50,
+        label="50x",
+        loc=4,
+        frameon=True,
+        pad=0.6,
+        sep=4,
+        linekw=dict(color="black", linewidth=0.5),
     )
-    i += 1
+    ax.add_artist(ob)
+    # fig.text(0.07,0.85,"Distribution of Aptitude Test Results from 18 – 24 year-olds",fontsize=20)
 
-df = pd.Series(final_speedups)
-df.rename("mcts", inplace=True)
-df.index.name = "benchmark"
-df.to_csv("mcts_speedups.csv")
-gs.update(hspace=-0.57)
-ob = AnchoredScaleBar(
-    size=50,
-    label="50x",
-    loc=4,
-    frameon=True,
-    pad=0.6,
-    sep=4,
-    linekw=dict(color="black", linewidth=0.5),
-)
-ax.add_artist(ob)
-# fig.text(0.07,0.85,"Distribution of Aptitude Test Results from 18 – 24 year-olds",fontsize=20)
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(directory.with_suffix(".pdf"), bbox_inches="tight")
+    df.to_csv(directory.with_suffix(".csv"))
 
-plt.tight_layout()
-# plt.show()
-plt.savefig("mcts_ridge.pdf", bbox_inches="tight")
+    # sns.color_palette("blend:#21599c,#b59610", as_cmap=True)
+    # sns.color_palette("dark:#5A9_r", as_cmap=True)
 
 
-# sns.color_palette("blend:#21599c,#b59610", as_cmap=True)
-# sns.color_palette("dark:#5A9_r", as_cmap=True)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("datadir")
+    args = parser.parse_args()
+    main(args.datadir)
