96dfb7ca76d472bed13b99bb760de65103e5998e
Emil VATAI <emil.vatai@gmail.com>
2025-10-02 14:04:45 +0900

    Hola

--- deps/install_scripts/all.sh
+++ deps/install_scripts/all.sh
@@ -3,7 +3,6 @@
 rm -rf "${TADASHI_DEPS_PREFIX:-$(git rev-parse --show-toplevel)/deps/opt}"
 SCRIPTS_DIR=${TADASHI_DEPS_PREFIX:-$(git rev-parse --show-toplevel)/deps/install_scripts}
 "${SCRIPTS_DIR}/gmp.sh"
-"${SCRIPTS_DIR}/llvm_src.sh"
+"${SCRIPTS_DIR}/llvm.sh"
 "${SCRIPTS_DIR}/python.sh"
 "${SCRIPTS_DIR}/swig.sh"
-
