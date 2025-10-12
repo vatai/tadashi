7aec448a955c70031f77501b62bc203292c5dd01
Emil VATAI <emil.vatai@gmail.com>
2025-10-12 15:24:31 +0900

    Include cmake

--- deps/install_scripts/all.sh
+++ deps/install_scripts/all.sh
@@ -7,3 +7,14 @@ SCRIPTS_DIR=${TADASHI_DEPS_PREFIX:-$(git rev-parse --show-toplevel)/deps/install
 "${SCRIPTS_DIR}/llvm.sh"
 "${SCRIPTS_DIR}/python.sh"
 "${SCRIPTS_DIR}/swig.sh"
+
+TADASHI_ROOT="$(git rev-parse --show-toplevel)"
+CMAKE_ARGS=(
+    -S "$TADASHI_ROOT"
+    -B "$TADASHI_ROOT/build"
+    -GNinja
+    -DCMAKE_INSTALL_PREFIX="$TADASHI_ROOT"/ctadashi
+    -DCALL_FROM_SETUP_PY=ON
+)
+cmake "${CMAKE_ARGS[@]}"
+ninja -C "$TADASHI_ROOT/build" install
