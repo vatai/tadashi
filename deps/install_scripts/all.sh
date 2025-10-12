c73462e62737fd4502c5f8663277b9e59b5e9faa
Emil VATAI <emil.vatai@gmail.com>
2025-10-12 15:34:00 +0900

    Source genoa in all.sh

--- deps/install_scripts/all.sh
+++ deps/install_scripts/all.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 
-rm -rf "${TADASHI_DEPS_PREFIX:-$(git rev-parse --show-toplevel)/deps/opt}"
-rm -rf "${BUILD_FILES:-$(git rev-parse --show-toplevel)/deps/build_files}"
-SCRIPTS_DIR=${TADASHI_DEPS_PREFIX:-$(git rev-parse --show-toplevel)/deps/install_scripts}
+TADASHI_ROOT="$(git rev-parse --show-toplevel)"
+rm -rf "${TADASHI_DEPS_PREFIX:-$TADASHI_ROOT/deps/opt}"
+rm -rf "${BUILD_FILES:-$TADASHI_ROOT/deps/build_files}"
+SCRIPTS_DIR=${TADASHI_DEPS_PREFIX:-$TADASHI_ROOT/deps/install_scripts}
+source "$TADASHI_ROOT/scripts/genoa.source"
 "${SCRIPTS_DIR}/gmp.sh"
 "${SCRIPTS_DIR}/llvm.sh"
 "${SCRIPTS_DIR}/python.sh"
 "${SCRIPTS_DIR}/swig.sh"
 
-TADASHI_ROOT="$(git rev-parse --show-toplevel)"
 CMAKE_ARGS=(
     -S "$TADASHI_ROOT"
     -B "$TADASHI_ROOT/build"
