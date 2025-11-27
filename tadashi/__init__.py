78d46a9a7be548f7ea54013213b73b77729414d4
Emil VATAI <emil.vatai@gmail.com>
2025-11-27 09:36:55 +0900

    Add decorator

--- tadashi/__init__.py
+++ tadashi/__init__.py
@@ -1,1 +1,31 @@
-# distutils: language=c++
+#!/bin/env python
+from enum import Enum, StrEnum, auto
+
+TRANSFORMATIONS = []  # : list["transformation"]
+
+
+class TrEnum(StrEnum):
+    INTERCHANGE = auto()
+
+
+def register(enum: TrEnum):
+    """Decorator to register transformations"""
+
+    def _decorator(cls):
+        TRANSFORMATIONS.append(cls)
+        return cls
+
+    return _decorator
+
+
+class TransformInfo:
+    pass
+
+
+@register(TrEnum.INTERCHANGE)
+class InterchangeInfo(TransformInfo):
+    pass
+
+
+print("=========================")
+print(f"{TRANSFORMATIONS=}")
