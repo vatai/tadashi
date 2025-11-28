b5421e66c27fafa3aa1db50084cb7ee1a41cede2
Emil VATAI <emil.vatai@gmail.com>
2025-11-29 08:16:46 +0900

    Return clang prefix and clang v17 for both

--- third_party/install.sh
+++ third_party/install.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 if [ -e "$(which yum)" ]; then
-	yum install -y gmp-c++ gmp-devel clang-devel-19.1.7 llvm-devel-19.1.7
+	yum install -y gmp-c++ gmp-devel clang-devel-17.0.6 llvm-devel-17.0.6
 elif [ -e "$(which apk)" ]; then
 	apk add gmp-dev clang17-dev llvm17-dev
 fi
@@ -11,6 +11,7 @@ fi
 ROOT="$(git rev-parse --show-toplevel)"
 THIRD_PARTY="$ROOT/third_party"
 PREFIX="$THIRD_PARTY/opt"
+CLANG_PREFIX="$(dirname "$(dirname "$(realpath "$(which llvm-config-17)")")")"
 
 cd "$THIRD_PARTY"
 git clone git://repo.or.cz/pet.git
@@ -18,5 +19,5 @@ git clone git://repo.or.cz/pet.git
 cd "$THIRD_PARTY/pet"
 ./get_submodules.sh
 ./autogen.sh
-./configure --prefix="$PREFIX"
+./configure --prefix="$PREFIX" --with-clang-prefix="$CLANG_PREFIX"
 make -j install
