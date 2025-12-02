8dcf91ff1acf6f5e110d0a25df078931f9677172
Emil VATAI <emil.vatai@gmail.com>
2025-12-02 12:14:28 +0900

    Move Simple app

--- old/tadashi/apps.py
+++ old/tadashi/apps.py
@@ -214,40 +214,6 @@ class App:
         return all(s.legal for s in self.scops)
 
 
-class Simple(App):
-    runtime_prefix: str
-
-    def __init__(
-        self,
-        source: str | Path,
-        compiler_options: Optional[list[str]] = None,
-        runtime_prefix: str = "WALLTIME: ",
-        ephemeral: bool = False,
-        populate_scops: bool = True,
-    ):
-        if compiler_options is None:
-            compiler_options = []
-        self.ephemeral = ephemeral
-        self.populate_scops = populate_scops
-        self.runtime_prefix = runtime_prefix
-        self._finalize_object(source, compiler_options=compiler_options)
-
-    @property
-    def compile_cmd(self) -> list[str]:
-        return [
-            self.compiler(),
-            str(self.source),
-            "-fopenmp",
-        ]
-
-    def extract_runtime(self, stdout) -> float:
-        for line in stdout.split("\n"):
-            if line.startswith(self.runtime_prefix):
-                num = line.split(self.runtime_prefix)[1]
-                return float(num)
-        return 0.0
-
-
 POLYBENCH_BASE = str(Path(__file__).parent.parent / "examples/polybench")
 
 
--- tadashi/apps.py
+++ tadashi/apps.py
@@ -1,9 +1,18 @@
 #!/bin/env python
 
+import abc
+from pathlib import Path
+from typing import Optional
+
 from .translators import Translator
 
 
-class BaseApp:
+class BaseApp(abc.ABC):
+
+    user_compiler_options: list[str]
+    ephemeral: bool = False
+    populate_scops: bool = True
+
     def __init__(self, source: str, translator: Translator):
         self.source = source
         self.translator = translator.set_source(source)
@@ -11,3 +20,37 @@ class BaseApp:
     @property
     def scops(self):
         return self.translator.scops
+
+
+class Simple(BaseApp):
+    runtime_prefix: str
+
+    def __init__(
+        self,
+        source: str | Path,
+        compiler_options: Optional[list[str]] = None,
+        runtime_prefix: str = "WALLTIME: ",
+        ephemeral: bool = False,
+        populate_scops: bool = True,
+    ):
+        if compiler_options is None:
+            compiler_options = []
+        self.ephemeral = ephemeral
+        self.populate_scops = populate_scops
+        self.runtime_prefix = runtime_prefix
+        self._finalize_object(source, compiler_options=compiler_options)
+
+    @property
+    def compile_cmd(self) -> list[str]:
+        return [
+            self.compiler(),
+            str(self.source),
+            "-fopenmp",
+        ]
+
+    def extract_runtime(self, stdout) -> float:
+        for line in stdout.split("\n"):
+            if line.startswith(self.runtime_prefix):
+                num = line.split(self.runtime_prefix)[1]
+                return float(num)
+        return 0.0
