9ad58f48074004928f90670d65377de6bfe95ce9
Emil VATAI <emil.vatai@gmail.com>
2025-12-26 10:48:47 +0900

    Add `test_non_existing_file`

--- old/_init_.py
+++ old/_init_.py
@@ -7,10 +7,3 @@ rtd = os.environ.get("READTHEDOCS")
 
 if rtd != "True":
     from ctadashi import ctadashi
-
-
-class Scops:
-    @staticmethod
-    def _check_missing_file(path: Path):
-        if not path.exists():
-            raise ValueError(f"{path} does not exist!")
