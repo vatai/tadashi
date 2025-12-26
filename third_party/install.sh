3be2a27954c129afbea583ec9f8b6b6c800a4ae4
Emil VATAI <emil.vatai@gmail.com>
2025-12-26 14:50:41 +0900

    Include llvm headers

--- third_party/install.sh
+++ third_party/install.sh
@@ -3,14 +3,24 @@ set -e
 set -x
 
 if [ -e "$(which yum)" ]; then
-	yum install -y gmp-c++ gmp-devel clang-devel-17.0.6 llvm-devel-17.0.6
+    yum install -y gmp-c++ gmp-devel clang-devel-17.0.6 llvm-devel-17.0.6
+    LICENSE_FILE=/usr/include/llvm/Support/LICENSE.TXT
 elif [ -e "$(which apk)" ]; then
-	apk add gmp-dev clang17-dev llvm17-dev
+    apk add gmp-dev clang17-dev llvm17-dev
+    LICENSE_FILE=/usr/lib/llvm17/include/llvm/Support/LICENSE.TXT
+elif [ -e "$(which apt-get)" ]; then
+    DEBIAN_FRONTEND=noninteractive
+    apt-get update
+    apt-get install -y git build-essential autoconf pkg-config libtool llvm-17-dev clang-17 libclang-17-dev libgmp-dev
+    LICENSE_FILE=/usr/lib/llvm-17/build/utils/lit/LICENSE.TXT
 fi
 
 ROOT="$(git rev-parse --show-toplevel)"
 THIRD_PARTY="$ROOT/third_party"
 PREFIX="$THIRD_PARTY/opt"
+
+cp -r "$(clang-17 -print-resource-dir)/include" "$ROOT/tadashi"
+cp "$LICENSE_FILE" "$ROOT/tadashi/include"
 CLANG_PREFIX="$(dirname "$(dirname "$(realpath "$(which llvm-config-17)")")")"
 CLANG_OPTION=${CLANG_PREFIX:+--with-clang-prefix=$CLANG_PREFIX}
 
