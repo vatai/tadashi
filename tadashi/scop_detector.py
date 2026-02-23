d7f34e81798d65b332c15f8ce4cfa22e5513c48b
Emil VATAI <emil.vatai@gmail.com>
2026-02-23 09:33:50 +0900

    Figure out argparse append

--- tadashi/scop_detector.py
+++ tadashi/scop_detector.py
@@ -1,2 +1,15 @@
+#!/bin/env python
+
+import argparse
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--foo", action="append")
+    args = parser.parse_args()
+    print(args)
     print("scopdetection start")
+
+
+if __name__ == "__main__":
+    main()
