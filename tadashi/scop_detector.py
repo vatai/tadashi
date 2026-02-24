8bf72d07ebc470ceab8a0f46116927eb2c51580f
Emil VATAI <emil.vatai@gmail.com>
2026-02-24 16:08:52 +0900

    WIP scop-detector

--- tadashi/scop_detector.py
+++ tadashi/scop_detector.py
@@ -1,14 +1,41 @@
 #!/bin/env python
 
 import argparse
+from pathlib import Path
+
+import tadashi.translators
+from tadashi import apps
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-i", "--pet", action="store_true")
+    group.add_argument("-l", "--polly", action="store_true", default=True)
+    parser.add_argument(
+        "-a",
+        "--translator_arg",
+        action="append",
+    )
+    parser.add_argument("files")
+    return parser.parse_args()
+
+
+def make_translator(args):
+    if args.pet:
+        translator = tadashi.translators.Pet(*args.translator_arg)
+    else:
+        polly_args = args.translator_arg
+        if not polly_args:
+            polly_args = ["flang"]
+        print(f"{polly_args=}")
+        translator = tadashi.translators.Polly(*polly_args)
+    return translator
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--foo", action="append")
-    args = parser.parse_args()
+    args = get_args()
     print(args)
-    print("scopdetection start")
 
 
 if __name__ == "__main__":
