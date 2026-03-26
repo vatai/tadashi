bc2b4dfb36ba33c6d8a4a91b9e5b1cd25b4bae1c
Emil VATAI <emil.vatai@gmail.com>
2026-03-26 11:22:48 +0900

    Remote execution but strange results

--- examples/tile_gemm_mpi4py.py
+++ examples/tile_gemm_mpi4py.py
@@ -14,18 +14,23 @@ def app_from_kwargs(kwargs):
     return Polybench(**kwargs)
 
 
-def remote_measure(kwargs, trs, tile_size):
+def remote_measure(kwargs, tile_size):
+    trs = [
+        [1, 2, TrEnum.FULL_SPLIT],
+        [1, 7, TrEnum.TILE_3D, tile_size, tile_size, tile_size],
+    ]
     print(f"{trs=}")
     hostname = socket.gethostname()
     print(f"{hostname=}")
     app = app_from_kwargs(kwargs)
     tapp = app.generate_code(alt_infix=f"_tiled{tile_size}{hostname}", ephemeral=False)
     tapp.compile()
-    rv = tapp.measure(repeat=5)
+    rv = tapp.measure()
     return rv
 
 
 def main():
+    print("[BEGIN]")
     kwargs = {
         "benchmark": "gemm",
         "base": "examples/polybench",
@@ -34,24 +39,24 @@ def main():
             "-DEXTRALARGE_DATASET",
         ],
     }
-    app = app_from_kwargs(kwargs)
 
     futures = []
     with MPIPoolExecutor() as executor:
-        for tile_size in [8, 15, 31]:
+        for tile_size in [19, 20, 21]:
             trs = [
                 [1, 2, TrEnum.FULL_SPLIT],
                 [1, 7, TrEnum.TILE_3D, tile_size, tile_size, tile_size],
             ]
-            future = executor.submit(remote_measure, kwargs, trs, tile_size)
+            future = executor.submit(remote_measure, kwargs, tile_size)
             futures.append(future)
 
+    app = app_from_kwargs(kwargs)
     app.compile()
     print(f"==== original: {app.measure()=}")
     for f in as_completed(futures):
-        print(f"**** {f.result(5)=}")
+        print(f"**** {f.result()=}")
 
-    print("DONE")
+    print("[END]")
 
 
 if __name__ == "__main__":
