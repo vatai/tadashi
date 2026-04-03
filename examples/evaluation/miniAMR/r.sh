8398b212dc4c16d832fae267a6532dd90d24525c
Emil VATAI <emil.vatai@gmail.com>
2026-04-03 10:48:31 +0900

    Move tadashi files to miniAMR repo

--- examples/evaluation/miniAMR/r.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=TADASHI       # create a short name for your job
-#SBATCH --nodes=1                # node count
-#SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
-
-source ../../../scripts/genoa/source
-
-PYTHONPATH=../../../ python3 🌳.py
