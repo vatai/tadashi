ae628cd0640977552b2973c0e15432b3767b30fa
Emil VATAI <emil.vatai@gmail.com>
2026-04-07 09:51:04 +0900

    Add wide sweep for tile gemm

--- examples/tile_gemm.py
+++ examples/tile_gemm.py
@@ -1,7 +1,7 @@
 import tadashi
 from tadashi import TrEnum
 from tadashi.apps import Polybench
-from tadashi.translators import Polly
+from tadashi.translators import Pet
 
 base = "examples/polybench"
 
@@ -9,13 +9,14 @@ gemm = Polybench(
     "linear-algebra/blas/gemm",
     base=base,
     compiler_options=["-DEXTRALARGE_DATASET", "-O3"],
-    translator=Polly(),
+    translator=Pet(),
 )
 print(f"{gemm.user_compiler_options=}")
 
 gemm.compile()
-print(f"{gemm.measure()=}")
-for tile_size in [31, 100]:
+otime = gemm.measure(5)
+print(f"==== {otime=}")
+for tile_size in range(8, 64):
     gemm.reset_scops()
     trs = [
         [0, 2, TrEnum.FULL_SPLIT],
@@ -29,10 +30,12 @@ for tile_size in [31, 100]:
         [0, 7, TrEnum.TILE_3D, tile_size, tile_size, tile_size],
     ]
     gemm.transform_list(trs)
-    print(gemm.scops[0].schedule_tree[7].yaml_str)
+    # print(gemm.scops[0].schedule_tree[7].yaml_str)
     tiled = gemm.generate_code(alt_infix=f"_tiled{tile_size}", ephemeral=False)
 
     tiled.compile()
-    print(f"{tile_size=} : {tiled.measure()=}")
+    ttime = tiled.measure()
+    speedup = otime / ttime
+    print(f"==== {tile_size=} : {otime=} : {ttime=} : {speedup=}")
 
 print("DONE")
