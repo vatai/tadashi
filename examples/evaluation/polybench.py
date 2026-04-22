d51cc14d92867b6bee2f2ca8924078746ff6907e
Emil VATAI <emil.vatai@gmail.com>
2026-04-22 16:57:02 +0900

    Fancy ml4t search

--- examples/evaluation/polybench.py
+++ examples/evaluation/polybench.py
@@ -4,20 +4,27 @@ import sys
 
 from tadashi.apps import Polybench
 
-ml4tadashi = os.path.dirname(__file__)
-ml4tadashi = os.path.dirname(ml4tadashi)
-ml4tadashi = os.path.dirname(ml4tadashi)
-sys.path.append(ml4tadashi)
-ml4tadashi = os.path.join(ml4tadashi, "ML4TADASHI")
-sys.path.append(ml4tadashi)
+def find_ml4t():
+    ML4T = "ML4TADASHI"
+    ml4tadashi = os.path.dirname(__file__)
+    while not os.path.exists(os.path.join(ml4tadashi, ML4T)):
+        ml4tadashi = os.path.dirname(ml4tadashi)
+    print(f"{ml4tadashi=}")
+    sys.path.append(ml4tadashi)
+    print(f"{sys.path=}")
+    ml4tadashi = os.path.join(ml4tadashi, ML4T)
+    sys.path.append(ml4tadashi)
+    print(f"{sys.path=}")
 
-from ML4TADASHI import run
+    global run
+    from ML4TADASHI import run
 
 tadashi_base = os.path.dirname(os.path.dirname(__file__))
 base = os.path.join(tadashi_base, "polybench")
 
 
 def main():
+    find_ml4t()
     parser = argparse.ArgumentParser()
     parser.add_argument("--benchmark")
     args, unknown = parser.parse_known_args()
