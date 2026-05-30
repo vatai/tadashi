069c827fdbbec78c5cf198b151c992386a69e1b0
Emil VATAI <emil.vatai@gmail.com>
2026-05-30 20:48:29 +0900

    Add comments about removed llvm commands

--- tadashi/translators.py
+++ tadashi/translators.py
@@ -350,10 +350,10 @@ class Polly(Translator):
         return [
             f"-polly-import-jscop-dir={self.tmpdir}",
             "-aa-pipeline=basic-aa",
-            "-polly-codegen",
-            # "-polly-use-llvm-names",
+            "-polly-codegen",  # moved from import jscop 3/3
+            # "-polly-use-llvm-names", # removed 1/3
             *options,
-            # "-polly-process-unprofitable",
+            # "-polly-process-unprofitable", # removed 2/3
         ]
 
     def _get_pre_polly_bc(self, options: list[str]) -> Path:
