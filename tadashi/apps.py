f09f8208da81a6b45da130781cd509c0990f590d
Emil VATAI <emil.vatai@gmail.com>
2026-06-08 21:28:47 +0900

    Better error?

--- tadashi/apps.py
+++ tadashi/apps.py
@@ -256,7 +256,7 @@ class App(abc.ABC):
         except Exception as e:
             if not safe:
                 raise
-            self.logger.critical(f"Exception:\n{e}\ntrs=\n{trs}\n")
+            self.logger.critical(f"Exception ({type(e)}):\n{e}\ntrs={trs}\n")
             wtime = math.inf
         return wtime, hostname
 
