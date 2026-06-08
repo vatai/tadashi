1b5461ec6bbd57bfeec857713849ad67635e201b
Emil VATAI <emil.vatai@gmail.com>
2026-06-08 15:42:21 +0900

    Fix job name

--- examples/evaluation/poc/generate_polybench_poc_pjsub.py
+++ examples/evaluation/poc/generate_polybench_poc_pjsub.py
@@ -118,7 +118,7 @@ def build_submission_script(args, config, benchmark, path):
         "--allow-omp" if args.allow_omp else "--no-allow-omp",
     ]
     return SUBMISSION_TEMPLATE.format(
-        job_name=f"EvoT_{config['name']}_{benchmark}",
+        job_name=f"poc_{config['name']}_{benchmark}",
         resource_group="small",
         elapse=args.elapse,
         env="\n".join(config["env"]),
