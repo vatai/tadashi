ef66fb41998610a9dea90c60cee021d33827fec6
Emil VATAI <emil.vatai@gmail.com>
2026-06-08 17:13:40 +0900

    Default true for allow omp

--- tadashi/apps.py
+++ tadashi/apps.py
@@ -424,7 +424,9 @@ class Polybench(App):
         parser.add_argument("--base", type=str, default="examples/polybench")
         parser.add_argument("--dataset", type=str, default="LARGE")
         parser.add_argument("--oflag", type=int, default=3)
-        parser.add_argument("--allow-omp", action=argparse.BooleanOptionalAction)
+        parser.add_argument(
+            "--allow-omp", action=argparse.BooleanOptionalAction, default=True
+        )
         return parser
 
     @classmethod
