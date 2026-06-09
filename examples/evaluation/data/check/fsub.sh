f76988855f4fa4a80d9165c82d0c9600c5888855
Emil VATAI <emil.vatai@gmail.com>
2026-06-09 16:23:50 +0900

    Remove -S

--- examples/evaluation/data/check/fsub.sh
+++ examples/evaluation/data/check/fsub.sh
@@ -6,6 +6,5 @@
 #PJM -L elapse=30:00
 #PJM -L node=1
 # #PJM --llio localtmp-size=40Gi
-#PJM -j -S
 module load LLVM/llvmorg-21.1.0
 python -u $ENTRYPOINT --benchmark="${BENCHMARK}" --root="${ROOT}"
