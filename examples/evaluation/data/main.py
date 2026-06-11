1b6889d194cb5aef499e9e23aa7b08593591a841
Emil VATAI <emil.vatai@gmail.com>
2026-06-11 14:16:30 +0900

    Codex good

--- examples/evaluation/data/main.py
+++ examples/evaluation/data/main.py
@@ -14,9 +14,9 @@ def check_ok(path: Path, benchmark: str, compiler: str):
     if len(check_files) != 1:
         print(f"{check_files=}")
     lines = check_files[0].read_text().split("\n")
-    if len(lines) != 8:
-        print(f"BAD CHECK {path}/{compiler}/{benchmark}")
-        # print("\n".join(lines))
+    # if len(lines) != 8:
+    #     # print(f"BAD CHECK {path}/{compiler}/{benchmark}")
+    #     # print("\n".join(lines))
     for i, line in enumerate(lines):
         if compiler in line:
             return lines[i + 1].startswith(">>> OK <<<")
@@ -24,19 +24,17 @@ def check_ok(path: Path, benchmark: str, compiler: str):
 
 
 def poc1(path: Path):
-    result = {}
+    result = defaultdict(dict)
     opattern = re.compile(r"Baseline measure: (.*)")
     tpattern = re.compile(r"Tiling with size ([^:]*): (.*)")
     # nt = path.name[:2]
     for p in path.rglob("*.stdout"):
-        entry = {}
         benchmark = p.parent.name
         compiler = p.parents[1].name
-        entry["compiler"] = compiler
         lines = p.read_text().split("\n")
         otime_str = lines[6]
-        if not check_ok(path, benchmark, compiler):
-            continue
+        entry = {}
+        entry["check"] = check_ok(path, benchmark, compiler)
         m = opattern.match(otime_str)
         if m:
             entry["otime"] = float(m.groups()[0])
@@ -46,8 +44,18 @@ def poc1(path: Path):
             g = m.groups()
             entry["tile_size"] = int(g[0])
             entry["ttime"] = float(g[1])
-        result[benchmark] = entry
+        result[compiler][benchmark] = entry
     return result
+
+
+def nested_data_frame(data):
+    return pd.concat(
+        {
+            compiler: pd.DataFrame.from_dict(benchmarks, orient="index")
+            for compiler, benchmarks in data.items()
+        },
+        names=["compiler", "benchmark"],
+    )
 
 
 def visit_st_mt(path: Path, fun):
@@ -57,9 +65,9 @@ def visit_st_mt(path: Path, fun):
     mt_data = fun(mt)
     for s in st_data:
         print(s)
-    st_df = pd.DataFrame(st_data)
-    mt_df = pd.DataFrame(mt_data)
-    return
+    st_df = nested_data_frame(st_data)
+    mt_df = nested_data_frame(mt_data)
+    return st_df, mt_df
 
 
 def get_pluto_data(path):
@@ -81,8 +89,12 @@ def get_poc_data(path):
 
 def main(pluto: Path, poc: Path, evo: Path, mcts: Path):
     # pluto_data = get_pluto_data(pluto)
-    poc_data = get_poc_data(poc)
-    print(poc_data)
+    st_poc, mt_poc = get_poc_data(poc)
+    print(st_poc)
+    print(mt_poc["check"].keys())
+    print(mt_poc.keys())
+    # print(poc_data["gemm"])
+    # print(len(poc_data.columns))
     # evo_data = get_evo_data(evo)
     # mcts_data = get_mcts_data(mcts)
 
