a0e2638cb3792fb7f55aa494ed47190214b6582c
Emil VATAI <emil.vatai@gmail.com>
2026-06-11 10:42:20 +0900

    Check all

--- examples/evaluation/data/check/fsub_all.sh
+++ examples/evaluation/data/check/fsub_all.sh
@@ -25,24 +25,25 @@ GCC_ARGS=(
 )
 
 for file in "${BENCHMARKS[@]}"; do
-	BM=$(basename ${file%.*})
-	pjsub -j -o evo-${BM}.%j \
-		-N evo-${BM} \
-		-x ENTRYPOINT=evo.py \
-		-x ROOT="$REPO_ROOT/../ML4TADASHI/scripts/after-safe-measure" \
-		-x BENCHMARK=$BM \
-		fsub.sh
-	pjsub -j -o mcts-${BM}.%j \
-		-N mcts-${BM} \
-		-x ENTRYPOINT=mcts.py \
-		-x ROOT="$REPO_ROOT/examples/evaluation/mcts/ro10k" \
-		-x BENCHMARK=$BM \
-		fsub.sh
-	pjsub -j -o poc-${BM}.%j \
-		-N poc-${BM} \
-		-x ENTRYPOINT=poc.py \
-		-x ROOT="$REPO_ROOT/examples/evaluation/poc/poc-runs" \
-		-x BENCHMARK=$BM \
-		fsub.sh
-
+	for NT in st mt; do
+		BM=$(basename ${file%.*})
+		pjsub -j -o evo/${BM}.%j \
+			-N evo/$NT-evo-${BM} \
+			-x ENTRYPOINT=evo.py \
+			-x ROOT="$REPO_ROOT/../ML4TADASHI/scripts/$NT-evo" \
+			-x BENCHMARK=$BM \
+			fsub.sh
+		pjsub -j -o mcts/$NT-mtcs-${BM}.%j \
+			-N mcts/${BM} \
+			-x ENTRYPOINT=mcts.py \
+			-x ROOT="$REPO_ROOT/examples/evaluation/mcts/$NT-mcts" \
+			-x BENCHMARK=$BM \
+			fsub.sh
+		pjsub -j -o poc/$NT-poc-${BM}.%j \
+			-N poc-${BM} \
+			-x ENTRYPOINT=poc.py \
+			-x ROOT="$REPO_ROOT/examples/evaluation/poc/$NT-poc" \
+			-x BENCHMARK=$BM \
+			fsub.sh
+	done
 done
