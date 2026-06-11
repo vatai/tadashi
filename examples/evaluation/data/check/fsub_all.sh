68e1a14e0d048edc2e0d5309940de3d5de1a9f61
Emil VATAI <emil.vatai@gmail.com>
2026-06-11 10:50:35 +0900

    Fixup

--- examples/evaluation/data/check/fsub_all.sh
+++ examples/evaluation/data/check/fsub_all.sh
@@ -28,20 +28,20 @@ mkdir -p evo mcts poc
 for file in "${BENCHMARKS[@]}"; do
 	for NT in st mt; do
 		BM=$(basename ${file%.*})
-		pjsub -j -o evo/${BM}.%j \
-			-N $NT-evo/$NT-evo-${BM} \
+		pjsub -j -o "evo/${NT}-evo-${BM}.%j" \
+			-N "${NT}-evo-${BM}" \
 			-x ENTRYPOINT=evo.py \
 			-x ROOT="$REPO_ROOT/../ML4TADASHI/scripts/$NT-evo" \
 			-x BENCHMARK=$BM \
 			fsub.sh
-		pjsub -j -o mcts/$NT-mtcs-${BM}.%j \
-			-N $NT-mcts/${BM} \
+		pjsub -j -o "mcts/${NT}-mtcs-${BM}.%j" \
+			-N "${NT}-mcts-${BM}" \
 			-x ENTRYPOINT=mcts.py \
 			-x ROOT="$REPO_ROOT/examples/evaluation/mcts/$NT-mcts" \
 			-x BENCHMARK=$BM \
 			fsub.sh
-		pjsub -j -o $NT-poc/$NT-poc-${BM}.%j \
-			-N poc-${BM} \
+		pjsub -j -o "poc/${NT}-poc-${BM}.%j" \
+			-N "${NT}-poc-${BM}" \
 			-x ENTRYPOINT=poc.py \
 			-x ROOT="$REPO_ROOT/examples/evaluation/poc/$NT-poc" \
 			-x BENCHMARK=$BM \
