9821c8de1562b1f6db2dc024b886fddc655b235b
Emil VATAI <emil.vatai@gmail.com>
2026-06-11 16:47:30 +0900

    check/fsub_all.sh remove unused variables

--- examples/evaluation/data/check/fsub_all.sh
+++ examples/evaluation/data/check/fsub_all.sh
@@ -3,8 +3,6 @@
 REPO_ROOT="$(realpath "$(git rev-parse --show-toplevel)")"
 PLUTO=${PLUTO:-$REPO_ROOT/deps/build/pluto-0.13.0/polycc}
 POLYBENCH_ROOT="$REPO_ROOT/examples/polybench"
-SIZE=EXTRALARGE
-OFLAG=3
 
 readarray -d '' BENCHMARKS < <(find "$POLYBENCH_ROOT" -name '*.c' |
 	grep -v polybench/utilities |
@@ -13,16 +11,6 @@ readarray -d '' BENCHMARKS < <(find "$POLYBENCH_ROOT" -name '*.c' |
 	grep -v orig.c |
 	grep -v pluto.c |
 	tr "\n" "\0")
-GCC_ARGS=(
-	"${POLYBENCH_ROOT}/utilities/polybench.c"
-	"-I${POLYBENCH_ROOT}/utilities"
-	"-DPOLYBENCH_TIME"
-	"-DPOLYBENCH_USE_RESTRICT"
-	"-D${SIZE}_DATASET"
-	"-O${OFLAG}"
-	"-lm"
-	"-fopenmp"
-)
 
 mkdir -p evo mcts poc
 for file in "${BENCHMARKS[@]}"; do
